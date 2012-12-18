3ce4a73f2bde36e6b81da2541b7f0b5635048156
Ian Ward <ian@excess.org>
2012-12-18 14:44:37 -0500

    first attempt at converting metadata schema

--- metadata_schema/data_gc_ca_2012/convert_schema.py
+++ metadata_schema/data_gc_ca_2012/convert_schema.py
@@ -12,16 +12,16 @@ import os
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 SCHEMA_NAME = os.path.join(HERE, 'metadata_schema.xml')
+LANGS = 'en', 'fr'
 
 schema_out = {
-    'fields': [],
+    'sections_fields': [],
     'bilingual_fields': [],
     }
 
 # ('section name', [field name 1, ...]), ...
 SECTIONS_FIELDS = [
     ("Metadata Record Information", [
-        'file_identifier',
         'author_email',
         ]),
     ("Dataset Identification Information", [
@@ -60,7 +60,6 @@ SECTIONS_FIELDS = [
 
 # '2012 field name': 'new field name'
 FIELD_MAPPING = {
-    'uniqueformid': 'file_identifier',
     'owner': 'author_email', # existing, was 'Contact'
     'contact_name': 'individual_name',
     'contact_title': 'position_name',
@@ -78,7 +77,7 @@ FIELD_MAPPING = {
     'program_page_en': 'program_url',
     'data_series_url_en': 'data_series_url',
     'dictionary_list:_en': 'data_dictionary',
-    'supplemantary_documentation_en': 'supplemental_information_other',
+    'supplementary_documentation_en': 'supplemental_information_other',
     'Geographic_Region_Name': 'geographic_region_name',
     'time_period_start': 'begin_position',
     'time_period_end': 'end_position',
@@ -95,14 +94,48 @@ BILINGUAL_FIELDS = {
     'program_url_fr': 'program_url', # note: different than english
     'data_series_url_fr': 'data_series_url',
     'data_dictionary_fr': 'data_dictionary', # note: different than english
-    'supplemantary_documentation_fr': 'supplemental_information_other',
+    'supplementary_documentation_fr': 'supplemental_information_other',
     'group_name_fr': 'data_series_name',
     }
 
 
-with open(SCHEMA_NAME) as s:
-    root = lxml.etree.parse(s)
+def main():
+    reverse_field_mapping = {v:k for k, v in FIELD_MAPPING.items()}
 
+    def lang_versions(xp):
+        """
+        Return {'en': english_text, 'fr': french_text} dict for a given
+        xpath xp.
+        """
+        out = {lang:root.xpath(xp + '[@xml:lang="%s"]' % lang)
+            for lang in LANGS}
+        assert out['en'], "Not found: %s" % xp
+        assert out['fr'], "Not found: %s" % xp
+        return {k:v[0].text for k, v in out.items()}
 
+    with open(SCHEMA_NAME) as s:
+        root = lxml.etree.parse(s)
+        schema_out['intro'] = lang_versions('//intro')
+
+        for section, fields in SECTIONS_FIELDS:
+            new_section = {
+                'section': {'en': section}, # FIXME: French version?
+                'fields': [],
+                }
+            for new_field in fields:
+                f = reverse_field_mapping[new_field]
+                xp = '//item[inputname="%s"]' % f
+                new_section['fields'].append({
+                    'id': new_field,
+                    'name': lang_versions(xp + '/name'),
+                    'help': lang_versions(xp + '/helpcontext'),
+                    'type': "".join(root.xpath(xp +
+                        '/type1/inputtype[1]/text()')),
+                    })
+            schema_out['sections_fields'].append(new_section)
+
+    return json.dumps(schema_out, sort_keys=True, indent=2)
+
+print main()
 
 
