2feb0f170866314c4ab57bc3e17cdbf1b6c0be84
Ian Ward <ian@excess.org>
2012-12-27 10:08:35 -0500

    metadata_schema: reverse FIELD_MAPPING and BILINGUAL_FIELDS dicts

--- metadata_schema/convert_schema.py
+++ metadata_schema/convert_schema.py
@@ -15,10 +15,6 @@ DATA_GC_CA_2012 = os.path.join(HERE, 'data_gc_ca_2012')
 SCHEMA_NAME = os.path.join(DATA_GC_CA_2012, 'metadata_schema.xml')
 LANGS = 'en', 'fr'
 
-schema_out = {
-    'sections_fields': [],
-    }
-
 # ('section name', [field name 1, ...]), ...
 SECTIONS_FIELDS = [
     ("Metadata Record Information", [
@@ -58,44 +54,44 @@ SECTIONS_FIELDS = [
         ]),
     ]
 
-# '2012 field name': 'new field name'
+# 'new field name': '2012 field name'
 FIELD_MAPPING = {
-    'owner': 'author_email', # existing, was 'Contact'
-    'contact_name': 'individual_name',
-    'contact_title': 'position_name',
-    'contact_phone': 'telephone_number_voice',
-    'contact_email': 'maintainer_email', # existing, was 'Email'
-    'title_en': 'title', # existing
-    'department': 'organization_name',
-    'category': 'thesaurus',
-    'language__': 'language',
-    'date_released': 'date',
-    'date_update': 'date_modified',
-    'frequency': 'maintenance_and_update_frequency',
-    'description_en': 'info', # existing, was 'Abstract'
-    'keywords_en': 'tags', # existing, was 'Keywords'
-    'program_page_en': 'program_url',
-    'data_series_url_en': 'url', # existing, was 'Data Series URL'
-    'dictionary_list:_en': 'data_dictionary',
-    'supplementary_documentation_en': 'supplemental_information_other',
-    'Geographic_Region_Name': 'geographic_region_name',
-    'time_period_start': 'begin_position',
-    'time_period_end': 'end_position',
-    'group_name_en': 'data_series_name',
+    'author_email': 'owner', # existing, was 'Contact'
+    'individual_name': 'contact_name',
+    'position_name': 'contact_title',
+    'telephone_number_voice': 'contact_phone',
+    'maintainer_email': 'contact_email', # existing, was 'Email'
+    'title': 'title_en', # existing
+    'organization_name': 'department',
+    'thesaurus': 'category',
+    'language': 'language__',
+    'date': 'date_released',
+    'date_modified': 'date_update',
+    'maintenance_and_update_frequency': 'frequency',
+    'info': 'description_en', # existing, was 'Abstract'
+    'tags': 'keywords_en', # existing, was 'Keywords'
+    'program_url': 'program_page_en',
+    'url': 'data_series_url_en', # existing, was 'Data Series URL'
+    'data_dictionary': 'dictionary_list:_en',
+    'supplemental_information_other': 'supplementary_documentation_en',
+    'geographic_region_name': 'Geographic_Region_Name',
+    'begin_position': 'time_period_start',
+    'end_position': 'time_period_end',
+    'data_series_name': 'group_name_en',
 # XXX doesn't seem right: 'number_datasets': 'issue_identification',
     }
 
 
-# '2012 French field name' : 'new corresponding english field name'
+# 'new field name' : '2012 French field name'
 BILINGUAL_FIELDS = {
-    'title_fr': 'title',
-    'description_fr': 'info',
-    'keywords_fr': 'tags',
-    'program_url_fr': 'program_url', # note: different than english
-    'data_series_url_fr': 'url',
-    'data_dictionary_fr': 'data_dictionary', # note: different than english
-    'supplementary_documentation_fr': 'supplemental_information_other',
-    'group_name_fr': 'data_series_name',
+    'title': 'title_fr',
+    'info': 'description_fr',
+    'tags': 'keywords_fr',
+    'program_url': 'program_url_fr', # note: different than english
+    'url': 'data_series_url_fr',
+    'data_dictionary': 'data_dictionary_fr', # note: different than english
+    'supplemental_information_other': 'supplementary_documentation_fr',
+    'data_series_name': 'group_name_fr',
     }
 
 def lang_versions(root, xp):
@@ -124,9 +120,9 @@ def data_gc_ca_2012_choices(name):
     return choices
 
 def main():
-    reverse_field_mapping = {v:k for k, v in FIELD_MAPPING.items()}
-    reverse_bilingual_fields = {v:k for k, v in BILINGUAL_FIELDS.items()}
-
+    schema_out = {
+        'sections_fields': [],
+        }
 
     with open(SCHEMA_NAME) as s:
         root = lxml.etree.parse(s)
@@ -138,7 +134,7 @@ def main():
                 'fields': [],
                 }
             for field in fields:
-                f = reverse_field_mapping[field]
+                f = FIELD_MAPPING[field]
                 xp = '//item[inputname="%s"]' % f
                 new_field = {
                     'id': field,
@@ -148,7 +144,7 @@ def main():
                     'type': "".join(root.xpath(xp +
                         '/type1/inputtype[1]/text()')),
                     }
-                old_id_fr = reverse_bilingual_fields.get(field, None)
+                old_id_fr = BILINGUAL_FIELDS.get(field, None)
                 if old_id_fr:
                     new_field['data_gc_ca_2012_id_fr'] = old_id_fr
                 new_field['bilingual'] = bool(old_id_fr)
