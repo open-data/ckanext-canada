8f9a1d17b9af9da85040c4be4d2ef4221feb45a2
Ian Ward <ian@excess.org>
2013-01-15 10:40:30 -0500

    python 2.6 compatibility

--- ckanext/canada/tests/metadata_schema.py
+++ ckanext/canada/tests/metadata_schema.py
@@ -7,11 +7,11 @@ class TestSchemaDescription(unittest.TestCase):
         self.sd = schema_description
 
     def test_schema_has_data(self):
-        self.assertGreater(len(self.sd.dataset_sections), 3)
-        self.assertGreater(len(self.sd.dataset_fields), 10)
-        self.assertGreater(len(self.sd.resource_fields), 3)
+        self.assertTrue(len(self.sd.dataset_sections) > 3)
+        self.assertTrue(len(self.sd.dataset_fields) > 10)
+        self.assertTrue(len(self.sd.resource_fields) > 3)
         for f in self.sd.dataset_sections:
-            self.assertGreater(len(f['fields']), 0)
+            self.assertTrue(len(f['fields']) > 0)
 
     # FIXME: remove this
     def test_old_attribute_names(self):
