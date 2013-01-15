29584dee8011ccc3d0e7c242c271b9f35cf69245
Ian Ward <ian@excess.org>
2013-01-15 10:23:59 -0500

    basic metadata_schema test

--- ckanext/canada/tests/metadata_schema.py
+++ ckanext/canada/tests/metadata_schema.py
@@ -6,6 +6,14 @@ class TestSchemaDescription(unittest.TestCase):
     def setUp(self):
         self.sd = schema_description
 
+    def test_schema_has_data(self):
+        self.assertGreater(len(self.sd.dataset_sections), 3)
+        self.assertGreater(len(self.sd.dataset_fields), 10)
+        self.assertGreater(len(self.sd.resource_fields), 3)
+        for f in self.sd.dataset_sections:
+            self.assertGreater(len(f['fields']), 0)
+
+    # FIXME: remove this
     def test_old_attribute_names(self):
         with self.assertRaises(AssertionError):
             self.sd.fields
