4363af167ac31e3b4ac0780abed5994b1bed54e2
Ian Ward <ian@excess.org>
2013-01-28 12:25:28 -0500

    pep8 for setup.py

--- setup.py
+++ setup.py
@@ -4,29 +4,29 @@ import sys, os
 version = '0.1.2'
 
 setup(
-	name='ckanext-canada',
-	version=version,
-	description="data.gc.ca CKAN extension",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Government of Canada',
-	author_email='Michel.Gendron@statcan.gc.ca',
-	url='',
-	license='MIT',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.canada'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
-        [ckan.plugins]
-	canada_public=ckanext.canada.plugins:DataGCCAPublic
-        canada_internal=ckanext.canada.plugins:DataGCCAInternal
-        canada_forms=ckanext.canada.plugins:DataGCCAForms
-	""",
+    name='ckanext-canada',
+    version=version,
+    description="data.gc.ca CKAN extension",
+    long_description="""
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Government of Canada',
+    author_email='Michel.Gendron@statcan.gc.ca',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.canada'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points=\
+    """
+    [ckan.plugins]
+    canada_public=ckanext.canada.plugins:DataGCCAPublic
+    canada_internal=ckanext.canada.plugins:DataGCCAInternal
+    canada_forms=ckanext.canada.plugins:DataGCCAForms
+    """,
 )
