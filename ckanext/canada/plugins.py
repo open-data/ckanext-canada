7d1cfa01eb1a75a0f53e4548887ee95a29058f96
Ian Ward <ian@excess.org>
2013-02-15 14:35:03 -0500

    clean up whitespace

--- ckanext/canada/plugins.py
+++ ckanext/canada/plugins.py
@@ -1,6 +1,6 @@
 import ckan.plugins as p
 from ckan.lib.plugins import DefaultDatasetForm
-import ckan.logic.schema as ckan_schema  
+import ckan.logic.schema as ckan_schema
 from ckan.lib.base import c, model
 import ckan.logic as logic
 import ckan.logic.schema as ckan_schema
@@ -44,24 +44,24 @@ class DataGCCAForms(p.SingletonPlugin, DefaultDatasetForm):
         """Returns a string representing the location of the template
         to be rendered. e.g. 'package/new_package_form.html'.
         """
-        # the form has been added to the template directory indicated in the 
+        # the form has been added to the template directory indicated in the
         # update_config() method of this class. Therefore, only the file name
         # is used and a sub-directory is not included.
         # **** TO DO ******  Edit this form
         return 'dataset_form.html'
 
     def is_fallback(self):
-        """Returns true iff this provides the fallback behaviour, when 
+        """Returns true iff this provides the fallback behaviour, when
         no other plugin instance matches a package's type.
-        
+
         There must be exactly one fallback controller definte, any attempt
-        to register more than one will throw an exception at startup. 
-        
-        If there's no fallback registered at startup the 
+        to register more than one will throw an exception at startup.
+
+        If there's no fallback registered at startup the
         ckan.lib.plugins.DefaultDatasetForm is used as the fallback.
         """
         return True
-    
+
     def package_types(self):
         """
         Returns an iterable of package type strings.
@@ -72,15 +72,15 @@ class DataGCCAForms(p.SingletonPlugin, DefaultDatasetForm):
         There must only be one plugin registered to each package type.  Any
         attempts to register more than one plugin instance to a given package
         type will raise an exception at startup.
-        
+
         The default value is 'dataset', as in 'http:.../dataset'.
         """
-        
+
         #return ["dataset"]
         return []
 
     def form_to_db_schema(self):
-        """Returns the schema for mapping package data from a form to 
+        """Returns the schema for mapping package data from a form to
         a format suitable for the database. A schema is a list that describes
         a dataset. See ``ckan.logic.schema``
         """
@@ -92,16 +92,16 @@ class DataGCCAForms(p.SingletonPlugin, DefaultDatasetForm):
 #            'composer_tags': [convert_to_tags(COMPOSER_VOCAB)]
 #        })
         return schema
-        
+
     def db_to_form_schema(self):
-        """Returns the schema for mapping package data from the database 
+        """Returns the schema for mapping package data from the database
         into a format suitable for the form (optional)
         """
         # return logic.schema.form_to_db_package_schema()
 #        schema = package_form_schema()
-        
+
         schema = ckan_schema.db_to_form_package_schema()
-        
+
 #        schema.update({
 #            'tags': {
 #                '__extras': [keep_extras, free_tags_only]
@@ -118,19 +118,19 @@ class DataGCCAForms(p.SingletonPlugin, DefaultDatasetForm):
 #            'name': [not_empty, unicode],
 #            'title': [ignore_missing]
 #        })
-        return schema        
-    
+        return schema
+
     def check_data_dict(self, data_dict, schema=None):
         """Check if the return data is correct.
         raise a DataError if not.
         """
         return
-    
+
     def setup_template_variables(self, context, data_dict=None):
         """Add variables to c just prior to the template being rendered.
         """
         DefaultDatasetForm.setup_template_variables(self, context, data_dict)
-    
+
     # Implement these methods if you wish to over-ride the default values of
     # - package/read.html
     # - package/search.html
