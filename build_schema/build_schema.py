22e460ac60b8b9cfee2c7effbc7a38e3e1206267
Ian Ward <ian@excess.org>
2013-03-11 13:43:17 -0400

    merge departments whose names changed slightly

--- build_schema/build_schema.py
+++ build_schema/build_schema.py
@@ -307,7 +307,7 @@ def apply_field_customizations(schema_out, vocab):
         def norm(t):
             if t == "Shapefile":
                 return "SHP"
-            return t.split('/')[0].strip()
+            return t.split('/')[0].strip()[:35]
         out = []
         ekeys = {}
         for d in c1:
