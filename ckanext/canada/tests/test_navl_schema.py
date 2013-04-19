bc9e2de6ea7e87875e2eac4998e8dcdbc12335a7
Ian Ward <ian@excess.org>
2013-04-19 16:47:20 -0400

    test validation failure with missing required fields

--- ckanext/canada/tests/test_navl_schema.py
+++ ckanext/canada/tests/test_navl_schema.py
@@ -1,12 +1,13 @@
-from ckan.tests import WsgiAppCase
+# -*- coding: UTF-8 -*-
+from ckan.tests import WsgiAppCase, CheckMethods
 import ckan.lib.search as search
 from ckan.lib.create_test_data import CreateTestData
 import ckan.model as model
 
-from ckanapi import TestAppCKAN
+from ckanapi import TestAppCKAN, ValidationError
 import json
 
-class TestNAVLSchema(WsgiAppCase):
+class TestNAVLSchema(WsgiAppCase, CheckMethods):
 
     @classmethod
     def setup_class(cls):
@@ -18,9 +19,9 @@ class TestNAVLSchema(WsgiAppCase):
             str(cls.sysadmin_user.apikey)).action
         cls.action = TestAppCKAN(cls.app).action
 
-    def test_basic_package(self, package_name=u'test_package', **kwargs):
+    def test_basic_package(self):
         package = {
-            'name': package_name,
+            'name': u'test_package',
             'title': u'A Novel By Tolstoy',
             'resources': [{
                 'description': u'Full text.',
@@ -28,11 +29,20 @@ class TestNAVLSchema(WsgiAppCase):
                 'url': u'http://www.annakarenina.com/download/'
             }]
         }
-        package.update(kwargs)
+
+        self.assert_raises(ValidationError,
+            self.sysadmin_action.package_create,
+            **package)
+
+        # fields we require
+        package['catalog_type'] = u'Data | Données'
+        package['title_fra'] = u'Un novel par Tolstoy'
+        package['maintenance_and_update_frequency'] = u'As Needed | Au besoin'
+        package['notes_fra'] = u'...'
 
         resp = self.sysadmin_action.package_create(**package)
-        assert resp['result']['title'] == u'A Novel By Tolstoy'
+        assert resp['result']['title_fra'] == u'Un novel par Tolstoy'
 
         resp = self.action.package_show(id=resp['result']['id'])
-        assert resp['result']['title'] == u'A Novel By Tolstoy'
+        assert resp['result']['title_fra'] == u'Un novel par Tolstoy'
 
