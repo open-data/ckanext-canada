52bf0d7bfe53bf9346207c31efd80e44950e8fc8
Ian Ward <ian@excess.org>
2014-04-16 13:08:57 -0400

    ati: add bounds for month for translation to text

--- ckanext/canada/ati.py
+++ ckanext/canada/ati.py
@@ -115,6 +115,7 @@ def _update_records(records, org_detail, conn):
         shortform = None
         shortform_fr = None
         ati_email = None
+        month = max(1, min(12, r['month']))
         for e in org_detail['extras']:
             if e['key'] == 'shortform':
                 shortform = e['value']
@@ -135,7 +136,7 @@ def _update_records(records, org_detail, conn):
                 .format(org),
             'ss_ati_disposition_en': r['disposition'].split(' / ', 1)[0],
             'ss_ati_month_en': '{0:02d}'.format(r['month']),
-            'ss_ati_monthname_en': calendar.month_name[r['month']],
+            'ss_ati_monthname_en': calendar.month_name[month],
             'ss_ati_number_of_pages_en': r['pages'],
             'ss_ati_organization_en': org_detail['title'].split(' | ', 1)[0],
             'ss_ati_year_en': r['year'],
@@ -154,7 +155,7 @@ def _update_records(records, org_detail, conn):
                 .format(org),
             'ss_ati_disposition_fr': r['disposition'].split(' / ', 1)[-1],
             'ss_ati_month_fr': '{0:02d}'.format(r['month']),
-            'ss_ati_monthname_fr': MONTHS_FRA[r['month']],
+            'ss_ati_monthname_fr': MONTHS_FRA[month],
             'ss_ati_number_of_pages_fr': r['pages'],
             'ss_ati_organization_fr': org_detail['title'].split(' | ', 1)[-1],
             'ss_ati_year_fr': r['year'],
