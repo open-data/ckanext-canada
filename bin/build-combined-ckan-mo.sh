8aea2313fa5fdd4319d27c5df5e745bcf62b8acf
Ross Thompson <ross.thompson@statcan.gc.ca>
2015-01-07 10:58:34 -0500

    BH 715: Ensure translation for accessibility text

--- bin/build-combined-ckan-mo.sh
+++ bin/build-combined-ckan-mo.sh
@@ -4,4 +4,5 @@ HERE=`dirname $0`
 msgcat --use-first \
     "$HERE/../i18n/fr/LC_MESSAGES/canada.po" \
     "$HERE/../../ckan/ckan/i18n/fr/LC_MESSAGES/ckan.po" \
+    "$HERE/../../ckanext-wet-boew/i18n/fr/LC_MESSAGES/wet_boew.po" \
     | msgfmt - -o "$HERE/../../ckan/ckan/i18n/fr/LC_MESSAGES/ckan.mo"
