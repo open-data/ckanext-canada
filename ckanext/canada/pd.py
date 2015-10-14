d573c5056a6554fabc1ab7545d0db0a758b5a454
Christopher Majewski <chris.majewski@gmail.com>
2015-10-14 12:14:44 -0400

    PD: Cleaner if for year extract

--- ckanext/canada/pd.py
+++ ckanext/canada/pd.py
@@ -222,7 +222,7 @@ def _update_records(records, org_detail, conn, recombinant_type):
                     value = date2zulu(value)
                     # CM: If this only applies to PD types this should be accurate
                     # CM: This should only apply if valid (as per date2zulu) else NULL
-                    if f.get('extract_date_year') == 'true':
+                    if f.get('extract_date_year'):
                         solrrec['date_year'] = value.split('-', 1)[0]
                 except ValueError:
                     pass
