2b8e448293eb6c23dc28b310a1f4fd3e1d26a6de
Ian Ward <ian@excess.org>
2015-12-07 16:33:10 -0500

    fix columntoggle with js workaround

--- ckanext/canada/public/static/js/dynamic_datatable.js
+++ ckanext/canada/public/static/js/dynamic_datatable.js
@@ -3,7 +3,24 @@ $(document).on("pageinit", function()
     var $table = $('#dtprv');
     var poll = setInterval( function(){
       if ($.fn.DataTable) {
-        $table.dataTable($.extend($table.data('wet-boew'), {bDestroy:true}));
+        $table.dataTable($.extend($table.data('wet-boew'), {
+          bDestroy:true,
+          fnDrawCallback: function(oSettings){
+            // http://stackoverflow.com/questions/16917605/jquerymobilerefresh-after-dynamically-adding-rows-to-a-table-with-column-toggle
+            var columnIndex = 0;
+            $("#dtprv-popup fieldset").find("input").each(function(){
+              var sel = ":nth-child(" + (columnIndex + 1) + ")";
+              $(this).jqmData("cells", $("#dtprv").find("tr").children(sel));
+              columnIndex++;
+              // with another terrible workaround
+              if (!this.checked) {
+                $(this).click();
+                $(this).click();
+              }
+            });
+            $('#dtprv').table('refresh');
+          }
+        }));
         clearInterval(poll);
       }
     }, 100 );
