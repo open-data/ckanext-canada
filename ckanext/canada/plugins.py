6a7b955cd079f467469d8524ef828d3f4b466c9d
Ian Ward <ian@excess.org>
2016-02-02 08:00:35 -0500

    Revert "hide ready_to_publish facet"
    This reverts commit 7889af20d8ec1a7c37acb1d43843c63d9d54c5c6.
    
    Conflicts:
    	ckanext/canada/templates/internal/package/search.html

--- ckanext/canada/plugins.py
+++ ckanext/canada/plugins.py
@@ -154,6 +154,7 @@ class DataGCCAPublic(p.SingletonPlugin):
                       'catalog_type': _('Data Type'),
                       'subject': _('Subject'),
                       'organization': _('Organization'),
+                      'ready_to_publish': _('Ready to Publish'),
                       'license_id': _('Licence') }
 
         return facets_dict
@@ -171,6 +172,7 @@ class DataGCCAPublic(p.SingletonPlugin):
                       'res_format': _('File Format'),
                       'catalog_type': _('Data Type'),
                       'subject': _('Subject'),
+                      'ready_to_publish': _('Ready to Publish'),
                       'license_id': _('Licence') }
 
         return facets_dict
