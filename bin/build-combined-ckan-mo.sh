f87e7164b160c5213acbfc675ba4389d8561306c
Laurent Goderre <laurent.goderre@gmail.com>
2016-03-11 08:33:31 -0500

    Moved the i18n inside the extension

--- bin/build-combined-ckan-mo.sh
+++ bin/build-combined-ckan-mo.sh
@@ -6,9 +6,9 @@ HERE=`dirname $0`
 DEST="$HERE/../build/i18n/fr/LC_MESSAGES/"
 mkdir -p $DEST
 msgcat --use-first \
-    "$HERE/../i18n/fr/LC_MESSAGES/canada.po" \
+    "$HERE/../ckanext/canada/i18n/fr/LC_MESSAGES/canada.po" \
     "$HERE/../../ckan/ckan/i18n/fr/LC_MESSAGES/ckan.po" \
-    "$HERE/../../ckanext-wet-boew/i18n/fr/LC_MESSAGES/wet_boew.po" \
+    "$HERE/../../ckanext-wet-boew/ckanext/wet_boew/i18n/fr/LC_MESSAGES/wet_boew.po" \
     | msgfmt - -o "$DEST/ckan.mo"
 
 # English overrides
@@ -16,5 +16,5 @@ HERE=`dirname $0`
 DEST="$HERE/../build/i18n/en/LC_MESSAGES/"
 mkdir -p $DEST
 msgcat --use-first \
-    "$HERE/../i18n/en/LC_MESSAGES/ckan.po" \
+    "$HERE/../ckanext/canada/i18n/en/LC_MESSAGES/ckan.po" \
     | msgfmt - -o "$DEST/ckan.mo"
