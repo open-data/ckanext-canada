2dc8948e8652444274e73c530455048ba3f7da7a
Ian Ward <ian@excess.org>
2016-03-24 15:29:13 -0400

    fix solr indexing of topic_category, spatial_representation_type

--- ckanext/canada/plugins.py
+++ ckanext/canada/plugins.py
@@ -266,7 +266,7 @@ class DataGCCAPackageController(p.SingletonPlugin):
         search_params['facet.range.start'] = 'NOW/DAY-100YEARS'
         search_params['facet.range.end'] = 'NOW/DAY+100YEARS'
         search_params['facet.range.gap'] = '+100YEARS'
-        
+
         return search_params
 
     def after_search(self, search_results, search_params):
@@ -280,10 +280,14 @@ class DataGCCAPackageController(p.SingletonPlugin):
     def before_index(self, data_dict):
         kw = json.loads(data_dict.get('extras_keywords', '{}'))
         data_dict['keywords'] = kw.get('en', [])
-        data_dict['keywords_fra'] = kw.get('fr')
+        data_dict['keywords_fra'] = kw.get('fr', [])
         data_dict['catalog_type'] = data_dict.get('type', '')
 
         data_dict['subject'] = json.loads(data_dict.get('subject', '[]'))
+        data_dict['topic_category'] = json.loads(data_dict.get(
+            'topic_category', '[]'))
+        data_dict['spatial_representation_type'] = json.loads(data_dict.get(
+            'spatial_representation_type', '[]'))
 
         if data_dict.get('portal_release_date'):
             data_dict.pop('ready_to_publish', None)
