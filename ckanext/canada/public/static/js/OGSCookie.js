a0dcf64a964d6f5026dc57eba4d0f924aa7756d5
Christopher Majewski <chris.majewski@gmail.com>
2016-05-20 11:53:18 -0400

    Adapt to OGP PHP not allowing setrawcookie
    PHP can only write encoded cookies, I have to decode it in JS.

--- ckanext/canada/public/static/js/OGSCookie.js
+++ ckanext/canada/public/static/js/OGSCookie.js
@@ -18,7 +18,7 @@ function readCookie(name) {
 	for(var i=0;i < ca.length;i++) {
 		var c = ca[i];
 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.search(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		if (c.search(nameEQ) == 0) return decodeURIComponent(c.substring(nameEQ.length,c.length));
 	}
 	return null;
 }
