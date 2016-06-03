f03419f806a4b52f5364b298bd5c76b7c8cc7263
Ian Ward <ian@excess.org>
2016-06-03 16:51:05 -0400

    correct download script name

--- bin/download_econ_code.py
+++ bin/download_econ_code.py
@@ -9,7 +9,7 @@ SOURCE_EN = 'http://www.tpsgc-pwgsc.gc.ca/recgen/pceaf-gwcoa/1617/fichiers-files
 SOURCE_FR = 'http://www.tpsgc-pwgsc.gc.ca/recgen/pceaf-gwcoa/1617/fichiers-files/rg-7-codes-fra.txt'
 
 
-def download_classification(url):
+def download_econ(url):
     response = requests.get(url, stream=True)
     csv = unicodecsv.reader(
         response.iter_lines(),
@@ -18,8 +18,8 @@ def download_classification(url):
     next(csv) # header row
     return dict(csv)
     
-fr = download_classification(SOURCE_FR)
-en = download_classification(SOURCE_EN)
+fr = download_econ(SOURCE_FR)
+en = download_econ(SOURCE_EN)
 
 for num in sorted(en):
     print json.dumps({'id': num, 'en': en[num], 'fr': fr[num]})
