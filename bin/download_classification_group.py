ca2236da95f8c425d7e9a3691ce29e91ea0396d0
Ian Ward <ian@excess.org>
2016-06-03 16:44:09 -0400

    download classification group script

---
+++ bin/download_classification_group.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import unicodecsv
+import requests
+import json
+import sys
+
+SOURCE_EN = 'http://www.tpsgc-pwgsc.gc.ca/recgen/pceaf-gwcoa/1617/fichiers-files/rg-7-codes-eng.txt'
+SOURCE_FR = 'http://www.tpsgc-pwgsc.gc.ca/recgen/pceaf-gwcoa/1617/fichiers-files/rg-7-codes-fra.txt'
+
+
+def download_classification(url):
+    response = requests.get(url, stream=True)
+    csv = unicodecsv.reader(
+        response.iter_lines(),
+        dialect='excel-tab',
+        encoding='utf-8')
+    next(csv) # header row
+    return dict(csv)
+    
+fr = download_classification(SOURCE_FR)
+en = download_classification(SOURCE_EN)
+
+for num in sorted(en):
+    print json.dumps({'id': num, 'en': en[num], 'fr': fr[num]})
+
+sys.stderr.write('wrote %d items\n' % len(en))
