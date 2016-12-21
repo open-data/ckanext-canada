2d9b804b5845aa1a374a8397888a95d97199995e
Ian Ward <ian@excess.org>
2016-12-21 12:45:49 -0500

    filter_disclosure_group script

---
+++ bin/filter_disclosure_group.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""
+Script that takes csv on stdin with an disclosure_group column
+and outputs the header row and all rows with disclosure_group
+!= "MPSES" (Minister/Parliamentary Secretaries/Exempt Staff)
+"""
+
+import csv
+import sys
+
+FILTER_COLUMN = "disclosure_group"
+
+def main():
+    reader = csv.DictReader(sys.stdin)
+    writer = csv.DictWriter(sys.stdout, reader.fieldnames)
+    writer.writerow(dict(zip(reader.fieldnames, reader.fieldnames)))
+    for row in reader:
+        try:
+            if row[FILTER_COLUMN] != 'MPSES':
+                writer.writerow(row)
+        except ValueError:
+            pass
+
+main()
