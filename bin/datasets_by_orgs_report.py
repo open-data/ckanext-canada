11338b3d5b401c6af20da9c34c70740bdc013e61
Ian Ward <ian@excess.org>
2017-01-03 15:25:11 -0500

    simple dump of current # datasets

--- bin/datasets_by_orgs_report.py
+++ bin/datasets_by_orgs_report.py
@@ -1,23 +1,46 @@
 #!/usr/bin/env python
 """
 Usage:
-
-datasets_by_orgs_report.py CKAN_URL MONTHS
+  datasets_by_orgs_report.py CKAN_URL MONTHS
 
 Eg:
-
-datasets_by_orgs_report.py http://open.canada.ca/data 12
+  datasets_by_orgs_report.py http://open.canada.ca/data 12 > report.csv
 """
+import sys
 
+from unicodecsv import DictWriter
 import ckanapi
 from docopt import docopt
 
-def main():
-    opts = docopt()
+UTF8_BOM = u'\uFEFF'.encode('utf-8')
 
+def main():
+    opts = docopt(__doc__)
+
+    num_months = int(opts['MONTHS'])
     rc = ckanapi.RemoteCKAN(opts['CKAN_URL'],
         user_agent='datasets_by_orgs_report.py (ckanext-canada)')
 
-    orgs = rc.action.organization_list(all_fields=True)
+    org_list = rc.action.organization_list(
+        all_fields=True,
+        include_dataset_count=True)
 
-    import pdb; pdb.set_trace()
+    orgs = {o['name']: o for o in org_list}
+    months = []
+
+    fieldnames = [u'id', u'title_en', u'title_fr', u'url', u'current_datasets']
+
+    sys.stdout.write(UTF8_BOM)
+    out = DictWriter(sys.stdout, fieldnames=fieldnames, encoding='utf-8')
+
+    for o in org_list:
+        out.writerow({
+            'id': o['id'],
+            'title_en': o['title'].split(' | ')[0],
+            'title_fr': o['title'].split(' | ')[-1],
+            'url': opts['CKAN_URL'].rstrip('/') + u'/organization/' + o['name'],
+            'current_datasets': o['package_count']
+            })
+
+
+main()
