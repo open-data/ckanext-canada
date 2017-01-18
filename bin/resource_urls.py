a73666382afe065dbcffa9daa0479b2bf5ffe789
Ian Ward <ian@excess.org>
2017-01-18 11:02:34 -0500

    bin/resource_urls.py: return only csv urls

--- bin/resource_urls.py
+++ bin/resource_urls.py
@@ -5,4 +5,5 @@ import sys
 
 source = ckanapi.RemoteCKAN(sys.argv[1])
 pkg = source.action.package_show(id=sys.argv[2])
-print ' '.join([r['url'] for r in pkg.get('resources', [])])
+print ' '.join([r['url'] for r in pkg.get('resources', [])
+    if r['url'].endswith('.csv')])
