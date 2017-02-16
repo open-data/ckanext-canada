093081397ffb31ffee59558141b015f669c76925
Jinfei Fan <fanjinfei@hotmail.com>
2017-02-16 10:19:34 -0500

    add BOM support for csv2xlsx

--- bin/csv2xlsx.py
+++ bin/csv2xlsx.py
@@ -5,11 +5,20 @@ Script that takes csv on stdin and writes an xlsx file on stdout
 """
 
 import csv
+import codecs
 import sys
 import openpyxl
 
+def peekBOM(f):
+    first3bytes = f.read(3)
+    if first3bytes == codecs.BOM_UTF8:
+        f.seek(3)
+    else:
+        f.seek(0)
+    return f
+
 def main():
-    reader = csv.reader(sys.stdin)
+    reader = csv.reader(peekBOM(sys.stdin))
     book = openpyxl.Workbook(write_only=True)
     sheet = book.create_sheet()
 
