1e8de3a74dccd031d7bb358a38bed3656a62ae88
Jinfei Fan <fanjinfei@hotmail.com>
2017-04-19 10:29:27 -0400

    add nothing to report resources

--- bin/upload_csv_from_datastore_tables.sh
+++ bin/upload_csv_from_datastore_tables.sh
@@ -62,13 +62,23 @@ ckanapi action resource_patch -c $REGISTRY_INI \
 ckanapi action resource_patch -c $REGISTRY_INI \
 	id=8282db2a-878f-475c-af10-ad56aa8fa72c upload@"${TMPDIR}/travelq.csv"
 ckanapi action resource_patch -c $REGISTRY_INI \
+	id=d3f883ce-4133-48da-bc76-c6b063d257a2 upload@"${TMPDIR}/travelq-nil.csv"
+ckanapi action resource_patch -c $REGISTRY_INI \
 	id=bdaa5515-3782-4e5c-9d44-c25e032addb7 upload@"${TMPDIR}/reclassification.csv"
+ckanapi action resource_patch -c $REGISTRY_INI \
+	id=1e955e4d-df35-4441-bf38-b7086192ece2 upload@"${TMPDIR}/reclassification-nil.csv"
 ckanapi action resource_patch -c $REGISTRY_INI \
 	id=7b301f1a-2a7a-48bd-9ea9-e0ac4a5313ed upload@"${TMPDIR}/hospitalityq.csv"
 ckanapi action resource_patch -c $REGISTRY_INI \
+	id=36a3b6cc-4f45-4081-8dbd-2340ca487041 upload@"${TMPDIR}/hospitalityq-nil.csv"
+ckanapi action resource_patch -c $REGISTRY_INI \
 	id=1d15a62f-5656-49ad-8c88-f40ce689d831 upload@"${TMPDIR}/grants.csv"
 ckanapi action resource_patch -c $REGISTRY_INI \
+	id=4e4db232-f5e8-43c7-b8b2-439eb7d55475 upload@"${TMPDIR}/grants-nil.csv"
+ckanapi action resource_patch -c $REGISTRY_INI \
 	id=fac950c0-00d5-4ec1-a4d3-9cbebf98a305 upload@"${TMPDIR}/contracts.csv"
+ckanapi action resource_patch -c $REGISTRY_INI \
+	id=fa4ff6c4-e9af-4491-9d4e-2b468e415a68 upload@"${TMPDIR}/contracts-nil.csv"
 ckanapi action resource_patch -c $REGISTRY_INI \
 	id=d0df95a8-31a9-46c9-853b-6952819ec7b4 upload@"${TMPDIR}/inventory.csv"
 ckanapi action resource_patch -c $REGISTRY_INI \
