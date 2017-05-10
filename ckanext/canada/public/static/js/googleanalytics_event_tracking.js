7683d4b554d3461bc482ea511a8233cdc49309fd
Tyler Kennedy <tk@tkte.ch>
2017-05-10 14:37:18 -0400

    GA tracking snippet moved to common location. GA tracking muted when on localhost. Resources updated with org/res/pkg IDs.

--- ckanext/canada/public/static/js/googleanalytics_event_tracking.js
+++ ckanext/canada/public/static/js/googleanalytics_event_tracking.js
@@ -1,7 +1,15 @@
 // Add Google Analytics Event Tracking to resource download links.
 jQuery('a.resource-url-analytics').on('click', function() {
-    var resource_url = encodeURIComponent(jQuery(this).prop('href'));
-    if (resource_url) {
-      _gaq.push(['_trackEvent', 'Resource', 'Download', resource_url]);
-    }
+  var resource_url = encodeURIComponent($(this).prop('href'));
+  if (resource_url) {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'resource',
+      eventAction: 'download',
+      eventLabel: resource_url,
+      dimension1: $(this).data('res-id'),
+      dimension2: $(this).data('pkg-id'),
+      dimension3: $(this).data('org-id')
+    });
+  }
 });
