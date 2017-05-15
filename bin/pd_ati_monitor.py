493ecbb1133f37ea1ca7c858986fcc497139add7
Jinfei Fan <fanjinfei@hotmail.com>
2017-05-15 12:32:25 -0400

    check file timestamp

--- bin/pd_ati_monitor.py
+++ bin/pd_ati_monitor.py
@@ -1,31 +1,51 @@
 import sys
 import subprocess
 import requests
+import time
 
 '''
 Usage: python pd_ati_monitor.py usr1@reg_host usr2@portal_host slack_url
 '''
 
-def sendSlack2(text, url):
+
+def send_slack(text, url):
     payload = {"text":text}
     count = 5
     while count >0:
         count -= 1
-        r = requests.post(url, json=payload)
-        if r.status_code == 200:
-            break
-    
+        try:
+            r = requests.post(url, json=payload)
+            if r.status_code == 200:
+                break
+        except (requests.exceptions.ProxyError,
+                requests.exceptions.ReadTimeout):
+            pass
+        time.sleep(5)
 
-def getCSVUpload(usrAtHost):
-    
-    cmd = 'ssh '+usrAtHost+' grep download /home/odatsrv/run_log/upload_ati_csv_from_datastore_tables.log | wc'
+
+def get_csv_upload(usr):
+    cmd = 'ssh ' + usr + ' stat -c %Y /home/odatsrv/run_log/upload_ati_csv_from_datastore_tables.log'
+    cmd = cmd.split(' ')
+    result = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+    now = time.time()
+    if now - int(result) > 3600 * 12:
+        return -1
+
+    cmd = 'ssh ' + usr + ' grep download /home/odatsrv/run_log/upload_ati_csv_from_datastore_tables.log | wc'
     cmd = cmd.split(' ')
     result = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
     return int(result.split()[0])
 
 
-def getPDUpdate(usrAtHost):
-    cmd = 'ssh '+usrAtHost+' grep Cleared /home/odatsrv/run_log/rebuild_pd_solr_from_uploaded_csv.log | wc'
+def get_pd_update(usr):
+    cmd = 'ssh '+ usr +' stat -c %Y /home/odatsrv/run_log/rebuild_pd_solr_from_uploaded_csv.log'
+    cmd = cmd.split(' ')
+    result = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+    now = time.time()
+    if now - int(result) > 3600 * 12:
+        return -1
+
+    cmd = 'ssh ' + usr + ' grep Cleared /home/odatsrv/run_log/rebuild_pd_solr_from_uploaded_csv.log | wc'
     cmd = cmd.split(' ')
     # Python3
     #result = subprocess.run(cmd, stdout=subprocess.PIPE)
@@ -35,27 +55,20 @@ def getPDUpdate(usrAtHost):
 
 
 def main():
-    usrAtReg = sys.argv[1]
-    usrAtPortal = sys.argv[2]
-    slackUrl = sys.argv[3]
-    if getCSVUpload(usrAtReg) ==15:
+    usr_reg = sys.argv[1]
+    usr_portal = sys.argv[2]
+    slack_url = sys.argv[3]
+    if get_csv_upload(usr_reg) ==15:
         text = ' csv ok,'
     else:
         text = ' csv bad,'
 
-    if getPDUpdate(usrAtPortal) ==9:
+    if get_pd_update(usr_portal) ==9:
         text += ' solr ok.'
     else:
         text += ' solr bad.'
 
-    count = 5
-    while count > 0:
-        count -= 1
-        try:
-            sendSlack2(text, slackUrl)
-            break
-        except:
-            pass
+    send_slack(text, slack_url)
 
 if __name__=='__main__':
     main()
