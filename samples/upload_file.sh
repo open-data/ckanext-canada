2be9c7c19f24fcd7932d6125990a9b0437ea087e
Jinfei Fan <fanjinfei@hotmail.com>
2017-05-30 12:07:14 -0400

    add french samples for obd

--- samples/upload_file.sh
+++ samples/upload_file.sh
@@ -11,3 +11,12 @@ ckanapi action resource_patch -c $CONFIG \
         id=ff58ac0f-f95f-4fb0-8674-f19fa21847ad upload@"./dq170529c-eng.pdf"
 ckanapi action resource_patch -c $CONFIG \
         id=758bff28-84fd-4ea7-9b70-5323aec7f61f upload@"./dq170529d-eng.pdf"
+
+ckanapi action resource_patch -c $CONFIG \
+        id=66013730-a9c3-48f0-86b9-caea70422f06 upload@"./dq170529b-fra.pdf"
+ckanapi action resource_patch -c $CONFIG \
+        id=9c53224a-d7a3-4053-9ddc-c454e5eac135 upload@"./dq170529a-fra.pdf"
+ckanapi action resource_patch -c $CONFIG \
+        id=c069a353-50d4-42f3-90bf-b5bcf4501aeb upload@"./dq170529c-fra.pdf"
+ckanapi action resource_patch -c $CONFIG \
+        id=77a51f91-6503-449e-9bc5-281b1ea924f8 upload@"./dq170529d-fra.pdf"
