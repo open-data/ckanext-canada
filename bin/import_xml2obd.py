3fccf05e508717f4538a842c3bf447b0c4dfd697
Jinfei Fan <fanjinfei@hotmail.com>
2017-06-19 15:06:01 -0400

    update obd xml import

--- bin/import_xml2obd.py
+++ bin/import_xml2obd.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 '''
 Usage:
-    import_xml2_obd.py <xml file> <organizations json file> <presets.yaml>
+    import_xml2_obd.py <xml file> <organizations json file> <presets.yaml> > <jsonl file>
+    import_xml2obd.py upload <site_url> <api_key> <jsonl file> <doc directory>
 '''
 import argparse
 import os
@@ -12,7 +13,12 @@ import json
 from collections import defaultdict, OrderedDict
 from lxml import etree
 import yaml
+import uuid
 
+import ckanapi
+import ckan
+from ckanapi.errors import CKANAPIError
+import traceback
 
 presets = None
 audience = None
@@ -25,6 +31,15 @@ def read_presets(filename):
     with open(filename, 'r') as f:
         _presets = yaml.load(f)
     return _presets
+
+
+def read_json(filename):
+    content=[]
+    with open(filename) as f:
+        for line in f:
+            rec = json.loads(line)
+            content.append(rec)
+    return content
 
 
 def read_xml(filename):
@@ -86,6 +101,8 @@ def read_organizations_from_json(filename):
 
 def owner_org(k,v):
     v = v.split('|')[0].strip()
+    if v =='Public Works and Government Services Canada':
+        v='Public Services and Procurement Canada'
     id = organizations.get(v, None)
     if not id:
         raise Exception('No such organiztion ' + v)
@@ -101,6 +118,17 @@ def _get_choices_value(preset, val):
                 res.append(item['value'])
                 break
     return name, res
+
+
+def _get_single_choices_value(preset, val):
+    name = preset['field_name']
+    res = []
+    for item in preset['choices']:
+        for label in item['label'].values():
+            if label in val:
+                res.append(item['value'])
+                break
+    return name, res[0]
 
 xml2obd=OrderedDict([
     ('Description English', desc),
@@ -121,7 +149,7 @@ xml2obd=OrderedDict([
 ])
 
 xml2resource={
-    'Resource Type': lambda k,v: _get_choices_value(canada_resource_type, v),
+    'Resource Type': lambda k,v: _get_single_choices_value(canada_resource_type, v),
     'Record Date': lambda k,v: ('created', v),
     'Unique ID': lambda k,v: ('unique_identifier', v),
     'Language': lambda k,v: _get_choices_value(canada_resource_language, v),
@@ -157,21 +185,105 @@ def get_preset(name):
     return None
 
 
+def upload_resources(remote_site, api_key, jsonfile, resource_directory):
+    #remote site
+    site = ckanapi.RemoteCKAN(
+        remote_site,
+        apikey=api_key,
+        user_agent='ckanapi-uploader/1.0')
+
+    #load dataset and resource file to cloud
+    ds = read_json(jsonfile)
+    for rec in ds:
+        try:
+            #site.action.package_delete(id=rec['id'])
+            #return
+            #site.action.package_create(**rec)
+            site.action.package_update(**rec)
+        except ckan.logic.NotFound:
+            try:
+                site.action.package_create(**rec)
+            except ckan.logic.ValidationError:
+                traceback.print_exc()
+                print(rec)
+                continue
+        except ckan.logic.ValidationError:
+            traceback.print_exc()
+            print(rec)
+            continue
+        res = rec['resources'][0]
+        source = resource_directory + '/' + res['url'].split('/')[-1]
+        with open(source) as f:
+            try:
+                rc = site.action.resource_patch(
+                    id=res['id'],
+                    upload=(os.path.basename(source), f))
+            except ckanapi.errors.CKANAPIError:
+                traceback.print_exc()
+                print(rec)
+                print("failed upload " + os.path.basename(source))
+                continue
+
+        print("updated for " + os.path.basename(source))
+
+
 def main():
     global presets, audience, canada_resource_type,canada_subject
     global canada_resource_language
+    canada_resource_format = None
+
+    # upload the generated json to remote site and upload the file
+    if sys.argv[1] =='upload':
+        return upload_resources(*sys.argv[2:])
+
     refs, extras = read_xml(sys.argv[1])
     read_organizations_from_json(sys.argv[2])
-    presets = read_presets(sys.argv[3])
+    presets = read_presets(sys.argv[3])  # TODO: read from doc.yaml
     audience = get_preset('canada_audience')
     canada_resource_type = get_preset('canada_resource_type')
     canada_subject = get_preset('canada_subject')
     canada_resource_language = get_preset('canada_resource_language')
+    canada_resource_format = map(lambda x:x['value'],
+                                 get_preset('canada_resource_format')['choices'])
 
     ds = xml_obd_mapping(refs, xml2obd)
     ds['collection'] = 'publication'
+    release_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if not ds.get('date_published'):
+        ds['date_published'] = release_date
+    ds['id'] = str(uuid.uuid5(uuid.NAMESPACE_URL,
+                   'http://dev.obd.ca/' + sys.argv[1]))
+    ds['name'] = ds['id']
+    ds['title'] = ds['title_translated']['en']
+    ds['state'] = 'active'
+    ds['type'] = 'doc'
+    ds['license_id'] = "ca-ogl-lgo"
+    if not ds.get('owner_org', None):
+        ds['owner_org'] = "A0F0FCFC-BC3B-4696-8B6D-E7E411D55BAC"
+    if not ds.get('keywords', None):
+        ds['keywords']={'en':['statcan'], 'fr':['statcan']}
+    if not ds.get('subject', None):
+        ds['subject'] = ["information_and_communications"]
+    if not ds.get('notes_translated', None):
+        ds['notes_translated'] = {'en':'n/a', 'fr':'n/a'}
+    if not ds['notes_translated'].get('fr', None):
+        ds['notes_translated']['fr'] = ds['notes_translated']['en']
+    if not ds['title_translated'].get('fr', None):
+        ds['title_translated']['fr'] = ds['title_translated']['en']
 
     res = xml_obd_mapping(refs, xml2resource)
+    res_name = sys.argv[1].split('/')[-1][:-4]
+    res['name_translated']={'en':res_name, 'fr':res_name}
+    res['id'] = str(uuid.uuid5(uuid.NAMESPACE_URL,
+                    'http://dev.obd.ca/resources/' + sys.argv[1]))
+    res['url'] = 'http://dev.obd.ca/' + sys.argv[1].split('/')[-1][:-4]
+    res['format'] = sys.argv[1].split('/')[-1].split('.')[-2].upper()
+    if res['format'] not in canada_resource_format:
+        res['format'] = 'other'
+    if not res.get('language'):
+        res['language'] = ['en']
+    if not res.get('resource_type'):
+        res['resource_type']='guide'
     ds['resources'] = [res]
     print(json.dumps(ds))
 
