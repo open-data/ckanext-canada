0657f678766979dfd10f60fd57a01e6d744bd042
Jinfei Fan <fanjinfei@hotmail.com>
2017-07-25 14:48:56 -0400

    handle network glitch on pd solr rebuild

--- bin/resource_urls.py
+++ bin/resource_urls.py
@@ -2,8 +2,17 @@
 
 import ckanapi
 import sys
+import time
 
-source = ckanapi.RemoteCKAN(sys.argv[1])
-pkg = source.action.package_show(id=sys.argv[2])
-print ' '.join([r['url'] for r in pkg.get('resources', [])
-    if r['url'].endswith('.csv')])
+retries = 0
+while retries< 5:
+    try:
+        source = ckanapi.RemoteCKAN(sys.argv[1])
+        pkg = source.action.package_show(id=sys.argv[2])
+        print ' '.join([r['url'] for r in pkg.get('resources', [])
+            if r['url'].endswith('.csv')])
+        sys.exit(0)
+    except ckanapi.errors.CKANAPIError:
+        retries += 1
+        time.sleep(8)
+sys.exit(-1)
