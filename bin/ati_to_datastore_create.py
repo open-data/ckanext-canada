7cc5fcf9ec918087a59c2e120091198c279b9d28
Ian Ward <ian@excess.org>
2017-09-11 10:30:59 -0400

    script for converting ati csv to datastore create params

---
+++ bin/ati_to_datastore_create.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import unicodecsv
+import json
+import sys
+
+sys.stdin.read(3)
+csv = unicodecsv.DictReader(sys.stdin, encoding='utf-8')
+
+print json.dumps({
+    'resource_id': '19383ca2-b01a-487d-88f7-e1ffbc7d39c2',
+    'fields': [
+        {'id':'year', 'type':'int'},
+        {'id':'month', 'type':'int'},
+        {'id':'request_number', 'type':'text'},
+        {'id':'summary_en', 'type':'text'},
+        {'id':'summary_fr', 'type':'text'},
+        {'id':'disposition', 'type':'text'},
+        {'id':'pages', 'type':'int'},
+        {'id':'owner_org', 'type':'text'},
+        {'id':'owner_org_title', 'type':'text'},
+    ],
+    'primary_key': ['owner_org', 'request_number'],
+    'indexes': ['year', 'month'],
+    'force': True,
+    'records': list(csv),
+})
