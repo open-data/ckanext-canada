1d9e32e4c3abeaa475f347f05545d44512e701d3
Ian Ward <ian@excess.org>
2018-02-22 13:11:50 -0500

    consultations filter

---
+++ bin/filter_consultations.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+Script that takes csv on stdin with a publishable column
+and outputs the header row and all rows with publishable
+= "Y". Columns publishable, record_created, record_modified
+and user_modified are removed from output.
+"""
+
+import csv
+import sys
+
+FILTER_COLUMN = "publishable"
+REMOVE_COLUMNS = [
+    'publishable',
+    'record_created',
+    'record_modified',
+    'user_modified',
+]
+
+def main():
+    reader = csv.DictReader(sys.stdin)
+    outnames = [f for f in reader.fieldnames if f not in REMOVE_COLUMNS]
+    writer = csv.DictWriter(sys.stdout, outnames)
+    writer.writeheader()
+    for row in reader:
+        try:
+            if row[FILTER_COLUMN] == 'Y':
+                for rem in REMOVE_COLUMNS:
+                    del row[rem]
+                writer.writerow(row)
+        except ValueError:
+            pass
+
+main()
