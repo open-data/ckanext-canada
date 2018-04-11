2dbf0bf1d438f0ba46f51c5bf580cd4e5be26808
Ian Ward <ian@excess.org>
2018-04-11 15:15:16 -0400

    accept more ugly dates

--- bin/migrate_grants_2018_04.py
+++ bin/migrate_grants_2018_04.py
@@ -4,7 +4,9 @@ import unicodecsv
 import sys
 import codecs
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+
+from openpyxl.utils.datetime import from_excel
 
 FIELDNAMES = 'project_identifier,amendment_number,amendment_date,agreement_type,recipient_type,recipient_business_number,recipient_legal_name,recipient_operating_name_en,recipient_operating_name_fr,research_organization_name_en,research_organization_name_fr,recipient_country,recipient_province,recipient_city_en,recipient_city_fr,recipient_postal_code,federal_riding_name_en,federal_riding_name_fr,federal_riding_number,prog_name_en,prog_name_fr,prog_purpose_en,prog_purpose_fr,agreement_title_en,agreement_title_fr,agreement_number,agreement_value,foreign_currency_type,foreign_currency_value,agreement_start_date,agreement_end_date,coverage_en,coverage_fr,description_en,description_fr,naics_identifier,expected_results_en,expected_results_fr,additional_information_en,additional_information_fr,owner_org,owner_org_title'.split(',')
 
@@ -16,15 +18,20 @@ out_csv.writeheader()
 
 def norm_date(d):
     "handle some creative thinking about what constitutes a date"
-    return d.replace('.', '-').strip()
+    d = d.replace('.', '-').strip()
+    for fmt in ['%Y-%m-%d', '%b-%d-%Y', '%m/%d/%Y']:
+        try:
+            return datetime.strptime(d, fmt)
+        except ValueError:
+            pass
+    return from_excel(int(d))
 
 for line in in_csv:
     try:
-        if datetime.strptime(norm_date(line['date']), '%Y-%m-%d'
-            ) >= datetime(2018, 4, 1):
+        if norm_date(line['date']) >= datetime(2018, 4, 1):
             raise ValueError
     except ValueError:
-        sys.stderr.write('skipping "{date}" {pid} {org}\n'.format(
+        sys.stderr.write('{org} {pid} "{date}"\n'.format(
             date=line['date'],
             pid=line['ref_number'],
             org=line['owner_org']))
@@ -47,7 +54,7 @@ for line in in_csv:
     line['agreement_number'] = (
         line.pop('prog_number') + '\t' + line.pop('proj_number')).strip()
     line['agreement_value'] = line.pop('value')
-    line['agreement_start_date'] = norm_date(line.pop('date'))
+    line['agreement_start_date'] = norm_date(line.pop('date')).strftime('%Y-%m-%d')
     line['additional_information_en'] = (
         line.pop('comments_en') + '\t' + line.pop('additional_info_en')).strip()
     line['additional_information_fr'] = (
