d66f725ddd2eeff0620e3b967d5f0281ef5a35d8
Ian Ward <ian@excess.org>
2018-05-01 11:39:47 -0400

    migrate contracts-nil

---
+++ bin/migrate_contracts_nil_2018_04.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import unicodecsv
+import sys
+import codecs
+
+FIELDNAMES = ['reporting_period', 'owner_org', 'owner_org_title']
+
+assert sys.stdin.read(3) == codecs.BOM_UTF8
+
+in_csv = unicodecsv.DictReader(sys.stdin, encoding='utf-8')
+out_csv = unicodecsv.DictWriter(sys.stdout, fieldnames=FIELDNAMES, encoding='utf-8')
+out_csv.writeheader()
+
+for line in in_csv:
+    year = int(line.pop('year'))
+    quarter = line.pop('quarter')
+    line['reporting_period'] = "%04d-%04d-%s" % (
+        year,
+        year + 1,
+        quarter)
+    out_csv.writerow(line)
