5feae6a24a4b40d5d041ea46583902e391956180
Ian Ward <ian@excess.org>
2018-07-03 13:03:58 -0400

    consultations: migrate partner_department numbers

--- bin/migrate/migrate_consultations_2018_07.py
+++ bin/migrate/migrate_consultations_2018_07.py
@@ -2,6 +2,7 @@
 
 import unicodecsv
 import sys
+import codecs
 
 FIELDNAMES = 'registration_number,publishable,partner_departments,subjects,title_en,title_fr,description_en,description_fr,target_participants_and_audience,start_date,end_date,status,profile_page_en,profile_page_fr,report_available_online,report_link_en,report_link_fr,contact_email,policy_program_lead_email,remarks_en,remarks_fr,rationale,record_created,record_modified,user_modified,owner_org,owner_org_title'.split(',')
 
@@ -20,4 +21,6 @@ for line in in_csv:
     line['end_date'] = line.pop('planned_end_date')
     line['profile_page_en'] = line.pop('further_information_en')
     line['profile_page_fr'] = line.pop('further_information_fr')
+    line['partner_departments'] = ','.join(
+        'D' + d for d in line['partner_departments'].split(',') if d)
     out_csv.writerow(line)
