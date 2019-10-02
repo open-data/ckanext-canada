2e37080d80b25939766bfe96f637d90a453f9bfc
Ian Ward <ian@excess.org>
2019-10-02 11:48:10 -0400

    reclassification: basic tests

---
+++ ckanext/canada/tests/test_reclassification.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+from nose.tools import assert_equal, assert_raises
+from ckanapi import LocalCKAN, ValidationError
+
+from ckan.tests.helpers import FunctionalTestBase
+from ckan.tests.factories import Organization
+
+from ckanext.recombinant.tables import get_chromo
+
+class TestReclassification(FunctionalTestBase):
+    def setup(self):
+        super(TestReclassification, self).setup()
+        org = Organization()
+        lc = LocalCKAN()
+        lc.action.recombinant_create(dataset_type='reclassification', owner_org=org['name'])
+        rval = lc.action.recombinant_show(dataset_type='reclassification', owner_org=org['name'])
+        self.resource_id = rval['resources'][0]['id']
+
+    def test_example(self):
+        lc = LocalCKAN()
+        record = get_chromo('reclassification')['examples']['record']
+        lc.action.datastore_upsert(
+            resource_id=self.resource_id,
+            records=[record])
+
+    def test_blank(self):
+        lc = LocalCKAN()
+        assert_raises(ValidationError,
+            lc.action.datastore_upsert,
+            resource_id=self.resource_id,
+            records=[{}])
