c23e264fe001b0e6b2e36e9a32c65042ffc39f77
RabiaSajjad <rabiasajjad.dev@gmail.com>
2019-12-09 15:26:51 -0500

    OPEN-164, OPEN-315: QP updates

--- bin/download_minister.py
+++ bin/download_minister.py
@@ -8,68 +8,158 @@ import sys
 import os.path
 from lxml import etree
 from operator import itemgetter
+from datetime import date
 
 OUTPUT_FILE = os.path.join(os.path.split(__file__)[0],
                            '../ckanext/canada/tables/choices/minister.json')
-choices = {}
+lang_codes = ["en", "fr"]
 
 
-def download_csv_filter_output(session_id):
+def get_filter_output():
     """
-    Read XML to generate JSON choices
+    Create a consolidated list of choices for ministers
     """
-    lang_codes = ["en", "fr"]
-    responses = {}
-    is_valid = False
+    existing_list = {}
 
+    # fetch existing list, if it exists
+    if os.path.isfile(OUTPUT_FILE):
+        with open(OUTPUT_FILE) as json_file:
+            try:
+                existing_list = json.load(json_file)
+            except ValueError as e:
+                return False
+        json_file.close()
+
+    # generate a list of choices from external source
+    choices = download_xml_from_source()
+
+    # if existing list does not exist
+    if not existing_list and choices:
+        return set_status(choices)
+    # if crawled choices list does not exist
+    elif existing_list and not choices:
+        return set_status(existing_list)
+    # if both exist, compare both lists for changes
+    elif existing_list and choices:
+        for row in existing_list:
+            if row in choices:
+                # case: minister found in both lists
+                # update minister if existing list is different from choices list
+                if cmp(existing_list[row], choices[row]):
+                    existing_list[row]['precedence'] = choices[row]['precedence']
+                    for langCode in lang_codes:
+                        existing_list[row][langCode] = choices[row][langCode]
+                        existing_list[row]['honorific_title_' + langCode] = choices[row]['honorific_title_' + langCode]
+                        # case: same minister with new position in choices, then append new position to existing list
+                        for position in choices[row]['positions_' + langCode]:
+                            if not [d for d in existing_list[row]['positions_' + langCode]
+                                    if d['title_' + langCode] == position['title_' + langCode]]:
+                                existing_list[row]['positions_' + langCode].append(position)
+                            else:
+                                # case: same minister with same position title but other details changed, then update
+                                idx = next((index for (index, d) in enumerate(existing_list[row]['positions_' + langCode]) if d['title_' + langCode] == position['title_' + langCode]), None)
+                                existing_list[row]['positions_' + langCode][idx].update(position)
+                        # case: same minister with existing position not in choices then update end date as today
+                        for position in existing_list[row]['positions_' + langCode]:
+                            if not [d for d in choices[row]['positions_' + langCode]
+                                    if d['title_' + langCode] == position['title_' + langCode]]:
+                                position['end_date'] = date.today().strftime("%Y-%m-%dT%H:%M:%S")
+            else:
+                # case: minister found in existing list but now in new list, update end date as today
+                for langCode in lang_codes:
+                    for position in existing_list[row]['positions_' + langCode]:
+                        position['end_date'] = date.today().strftime("%Y-%m-%dT%H:%M:%S")
+
+        for row in choices:
+            if row not in existing_list:
+                # case: new minister in choices, add new minister to existing list
+                existing_list[row] = choices[row]
+
+        return set_status(existing_list)
+    # both lists do not exist
+    else:
+        return False
+
+
+def download_xml_from_source():
+    """
+    Read XML from external source to generate JSON choices for ministers
+    """
+    choices = {}
+    xml_tree = {}
+
+    # collect responses for both languages
     for langCode in lang_codes:
-        url = 'https://www.ourcommons.ca/members/' + langCode + '/ministries/xml?ministry=' + str(session_id)
+        # url = 'https://www.ourcommons.ca/Parliamentarians/' + langCode + '/ministries/Export?output=XML'
+        url = 'http://localhost/export-' + langCode + '.xml'
         response = requests.get(url, stream=True)
-        responses[langCode] = response
 
-    if responses['en'].status_code == 200 and responses['fr'].status_code == 200:
-        xml_en = etree.fromstring(responses['en'].content)
-        xml_fr = etree.fromstring(responses['fr'].content)
+        # if valid response then save as xml tree
+        if response.status_code == 200:
+            xml_tree[langCode] = etree.fromstring(response.content)
 
-        for row in xml_en.xpath('Minister'):
-            is_valid = True
-            name = row.xpath('PersonOfficialFirstName/text()')[0] + ' ' + row.xpath('PersonOfficialLastName/text()')[0]
-            choices[name] = {
-                'en': row.xpath('PersonShortHonorific/text()')[0] + ' ' + name + ' - ' + row.xpath('Title/text()')[0],
-                'precedence': row.xpath('OrderOfPrecedence/text()')[0],
-                'title_en': row.xpath('Title/text()')[0],
-                'honorific_title_en': row.xpath('PersonShortHonorific/text()')[0],
-                'start_date': row.xpath('FromDateTime/text()')[0],
-                'end_date': '' . join(row.xpath('ToDateTime/text()')),
-            }
+            for row in xml_tree[langCode].xpath('Minister'):
+                # gather name and position
+                name = row.xpath('PersonOfficialFirstName/text()')[0] + ' ' + \
+                       row.xpath('PersonOfficialLastName/text()')[0]
+                position = {
+                    'title_' + langCode: row.xpath('Title/text()')[0],
+                    'start_date': row.xpath('FromDateTime/text()')[0],
+                    'end_date': ''.join(row.xpath('ToDateTime/text()'))
+                }
+                # if name already exist in json
+                if name in choices:
+                    # add translation
+                    if langCode not in choices[name]:
+                        choices[name].update([
+                            (langCode, row.xpath('PersonOfficialLastName/text()')[0] + ', '
+                             + row.xpath('PersonOfficialFirstName/text()')[0]
+                             + ' (' + row.xpath('PersonShortHonorific/text()')[0] + ')'),
+                            ('honorific_title_' + langCode, row.xpath('PersonShortHonorific/text()')[0]),
+                            ('positions_' + langCode, [position]),
+                            ])
+                    else:
+                        # append position
+                        choices[name]['positions_' + langCode].append(position)
 
-            for row_fr in xml_fr.xpath('Minister'):
-                name_fr = row_fr.xpath('PersonOfficialFirstName/text()')[0] \
-                          + ' ' \
-                          + row_fr.xpath('PersonOfficialLastName/text()')[0]
-                if name == name_fr:
-                    choices[name].update([('fr', row_fr.xpath('PersonShortHonorific/text()')[0] + ' '
-                                           + name + ' - ' + row_fr.xpath('Title/text()')[0]),
-                                          ('title_fr', row_fr.xpath('Title/text()')[0]),
-                                          ('honorific_title_fr', row_fr.xpath('PersonShortHonorific/text()')[0])])
-                    break
-
-    for langCode in lang_codes:
-        responses[langCode].close()
-
-    return is_valid
+                # new name in json
+                else:
+                    choices[name] = {
+                        langCode: row.xpath('PersonOfficialLastName/text()')[0] + ', '
+                        + row.xpath('PersonOfficialFirstName/text()')[0]
+                        + ' (' + row.xpath('PersonShortHonorific/text()')[0] + ')',
+                        'honorific_title_' + langCode: row.xpath('PersonShortHonorific/text()')[0],
+                        'precedence': row.xpath('OrderOfPrecedence/text()')[0],
+                        'positions_' + langCode: [position],
+                    }
+        response.close()
+    return choices
 
 
-# first session for publishing according to Bill C-58
-sessionId = 28
-while download_csv_filter_output(sessionId):
-    sessionId += 1
+def set_status(list):
+    """
+    Set status for list to determine if current vs former minister
+    """
+    status = 0
+    for row in list:
+        for position in list[row]['positions_en']:
+            if not position['end_date']:
+                status = 1
+                break
+        list[row]['status'] = status
+    return list
 
-open(OUTPUT_FILE, 'wb').write(json.dumps(
-    choices,
-    indent=2,
-    separators=(',', ':'),
-    sort_keys=True,
-    ensure_ascii=False,
-).encode('utf-8'))
-sys.stderr.write('wrote %d items\n' % len(choices))
+
+# get list of choices and write to OUTPUT file as JSON
+minister_list = get_filter_output()
+if minister_list:
+    open(OUTPUT_FILE, 'wb').write(json.dumps(
+        minister_list,
+        indent=2,
+        separators=(',', ':'),
+        sort_keys=True,
+        ensure_ascii=False,
+    ).encode('utf-8'))
+    sys.stderr.write('wrote %d items\n' % len(minister_list))
+else:
+    sys.stderr.write('Unable to create ministers list\n')
