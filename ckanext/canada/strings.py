a0f7e70702feac8c5d49a6573d4c74239f52e177
Ian Ward <ian@excess.org>
2020-01-17 16:17:22 -0500

    contracts: more validation, article6, award criteria pending

--- ckanext/canada/strings.py
+++ ckanext/canada/strings.py
@@ -1,3 +1,4 @@
+# coding=UTF-8
 """
 Copies of strings that need translations but babel can't find, e.g. text embedded in triggers
 """
@@ -9,3 +10,8 @@ _("If the value XX (none) is entered, then no other value can entered in this fi
 _("If the value NA (not applicable) is entered, then no other value can entered in this field.")
 _("This field must be NA (not applicable) if the Agreement Type or Trade Agreement field is not 0 (none) or XX (none), as applicable.")
 _("If the value 00 (none) is entered, then no other value can be entered in this field.")
+_("This field must contain the first three digits of a postal code in A1A format or the value “NA”")
+_("This field must be N, No or Non, if the Agreement Type or Trade Agreement field is not 0 (none) or XX (none), as applicable.")
+_("This field must be N, No or Non, if the Procurement Strategy for Aboriginal Business field is MS or VS.")
+_("This field may only be populated with a 1 if the solicitation procedure is identified as non-competitive (TN) or Advance Contract Award Notice (ACAN)")
+_("This field may only be populated with a 0 if a Call-up against a standing offer or supply arrangement was identified in the Contracting Entity data field")
