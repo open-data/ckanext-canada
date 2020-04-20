28ad5096721741746b98328b767c93cf2b393b42
Rabia Sajjad <rabiasajjad.dev@gmail.com>
2020-04-20 17:24:34 -0400

    ga: add event tracking for recombinant

--- ckanext/canada/internal/static/registry_ga_event_tracking.js
+++ ckanext/canada/internal/static/registry_ga_event_tracking.js
@@ -1,34 +1,46 @@
 /*
  Add Google Analytics Event Tracking to actions on the Registry
  */
-function trackEvent(id, category, action = undefined) {
-  let element = document.getElementById(id);
-  if (element) {
-    if (action === undefined) {
-      var headings = document.getElementsByTagName('H1');
-      action = headings.length > 0
-          ? headings[0].innerHTML
-          : element.innerHTML;
-    }
-
-    let url = null;
-    if (element.tagName === 'BUTTON')
-      url = element.formAction;
-    else if (element.tagName === 'A')
-      url = element.href;
-
-    if (category && action && url) {
-      ga('send', {
-        hitType: 'event',
-        eventCategory: category,
-        eventAction: action,
-        eventLabel: url,
-      });
-    }
-  }
-}
-
 window.onload = function() {
+  let flashes = document.getElementsByTagName('aside');
+  if (flashes && flashes[0].innerText) {
+    let msg = flashes[0].innerText;
+
+    // track successful login
+    if (msg.match(/.*now logged in$/) || msg.match(/.*est maintenant connecté$/))
+      ga('send', 'event', 'Login', msg, document.referrer);
+
+    // track password reset request
+    if (msg == 'Please check your e-mail inbox in order to confirm your password reset request.' ||
+        msg == 'S.V.P. consulter votre boîte à lettres électronique pour confirmer votre demande de réinitialisation.')
+      ga('send', 'event', 'Login', 'Reset password', document.location.href);
+
+    // track account created
+    if (msg.match(/^Account Created.*/) || msg.match(/^Compte créé.*/))
+      ga('send', 'event', 'Login', 'Request an Account', document.referrer);
+
+    // track single record create for recombinant type
+    if (msg == 'Record Created')
+      ga('send', 'event', document.getElementsByTagName('H1')[0].innerText, msg, document.location.href);
+
+    // track single record updated for recombinant type
+    if (msg.match(/^Record.*Updated$/))
+      ga('send', 'event', document.getElementsByTagName('H1')[0].innerText, msg, document.location.href);
+
+    // track Excel spreadsheet uploaded for recombinant type
+    if (msg == 'Your file was successfully uploaded into the central system.' ||
+        msg == 'Votre fichier a été téléchargé avec succès dans le système central.')
+      ga('send', 'event', document.getElementsByTagName('H1')[0].innerText, 'Spreadsheet uploaded', document.location.href);
+  }
+
+  // track invlid login
   if (document.getElementById('login-invalid'))
-    trackEvent('login-invalid', 'login', 'Invalid login');
+    ga('send', 'event', 'Login', 'Invalid login', document.location.href);
+
+  // track Excel spreadsheet uploaded errors for recombinant type
+  if (document.getElementById('upload-errors')) {
+    let msg = document.getElementById('upload-errors').innerText;
+    if (msg)
+      ga('send', 'event', document.getElementsByTagName('H1')[0].innerText, 'Spreadsheet upload errored ' + msg, document.location.href);
+  }
 };
