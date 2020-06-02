78b2958cf37348178e7c586c01e47030a1f5b6a3
maxFredenburgh <maxwellfredenburgh.dev@gmail.com>
2020-06-02 09:44:36 -0400

    OPN-57 automated warehouse feature

--- bin/warehouse/csv_diff.py
+++ bin/warehouse/csv_diff.py
@@ -8,7 +8,7 @@ from sys import stderr, stdout, argv
 
 def get_row_tuple(row):
     org_tuple = (row['owner_org'], row['owner_org_title'])
-    del row['owner_org']
+    #del row['owner_org']
     del row['owner_org_title']
     pk_tuple = tuple([row[key] for key in pk_fields[0]])
     h = hashlib.md5(repr(list(row.values())).encode('utf8')).digest()
@@ -64,12 +64,12 @@ def add_metadata_fields(created_rows, modified_rows, deleted_rows, prev_csv, dat
     results = []
     for cr in created_rows:
         cr['log_date'] = datestamp
-        cr['log_activity'] = 'Created'
+        cr['log_activity'] = 'C'
         results.append(cr)
 
     for mr in modified_rows:
         mr['log_date'] = datestamp
-        mr['log_activity'] = 'Modified'
+        mr['log_activity'] = 'M'
         results.append(mr)
 
     with open(prev_csv) as f:
@@ -79,15 +79,14 @@ def add_metadata_fields(created_rows, modified_rows, deleted_rows, prev_csv, dat
                 row_tuple = get_row_tuple(row)
                 if t[0] == row_tuple[0] and t[1] == row_tuple[1]:
                     row['log_date'] = datestamp
-                    row['log_activity'] = 'Deleted'
+                    row['log_activity'] = 'D'
                     results.append(row)
                     break
-
     return results
 
 
 def get_fieldnames(fields):
-    fieldnames = ",".join([f['id'] for f in fields])+",log_date,log_activity"
+    fieldnames = ",".join([f['id'] for f in fields])+",owner_org,log_date,log_activity"
     return fieldnames
 
 
@@ -103,15 +102,26 @@ else:
     fields = [f['fields'] for f in field_info['resources'] if 'nil' not in f['resource_name']]
 
 fieldnames = get_fieldnames(fields[0]).split(",")
+print(fieldnames)
 existing_rows = collect_existing_rows(prev_csv)
+print("Comparing...")
 created_rows, modified_rows, deleted_rows = compare(current_csv, existing_rows)
 results = add_metadata_fields(created_rows, modified_rows, deleted_rows, prev_csv, datestamp)
 
+print("writing")
 if results:
-    with open(outfile, 'w') as f:
+    with open(outfile, 'a') as f:
         warehouse = csv.DictWriter(f, fieldnames=fieldnames, delimiter=',', restval='')
         warehouse.writeheader()
         for result_row in results:
+            for key in result_row.keys():
+                if key not in fieldnames:
+                    result_row.pop(key)
+                if key == 'record_created':
+                    result_row['record_created'] = (result_row['record_created'].split('T')[0]).replace('/','-')
+                if key == 'record_modified':
+                    result_row['record_modified'] = (result_row['record_modified'].split('T')[0]).replace('/','-')
+                print(result_row)
             warehouse.writerow(result_row)
 
 else:
---
+++ bin/warehouse/generate_warehouse.py
@@ -0,0 +1,85 @@
+import tarfile
+import sys
+import os
+import subprocess
+import shutil
+from datetime import datetime
+
+fname = sys.argv[1]
+
+tar_array = sorted(os.listdir(fname))
+print(tar_array)
+prev = ''
+curr = ''
+
+def get_base(tfile):
+    base = os.path.basename(tfile)
+    pd_name = os.path.splitext(os.path.splitext(base)[0])[0]
+    return pd_name
+
+def extract(tfile,dest):
+    fpath = './' + dest
+    tar = tarfile.open(fname + tfile)
+    tar.extractall(path=fpath)
+    tar.close()
+    return fpath
+
+def run_migrations(fpath):
+    if not os.path.exists('temp'):
+        os.mkdir('temp')
+    for csvfile in os.listdir(fpath):
+        print("Migrating {0} from directory {1}".format(csvfile, fpath))
+        proc = subprocess.Popen(['python', 'migrate_all.py', fpath+'/'+csvfile, 'temp/'+fpath+'m_'+csvfile])
+        proc.wait()
+
+def csv_diff(prev_csv, curr_csv, endpoint, outfile):
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d")
+
+    print("Getting difference between {0} and {1}".format(prev_csv,curr_csv))
+    proc = subprocess.Popen(['python', 'csv_diff.py', 'temp/'+prev_csv, 'temp/'+curr_csv, endpoint,
+                             dt_string,outfile])
+    proc.wait()
+
+#tar_array = tar_array[-2:]
+
+while tar_array:
+    if tar_array == []:
+        break
+    if prev == '':
+        prev = tar_array.pop(0)
+        curr = tar_array.pop(0)
+    else:
+        prev = curr
+        curr = tar_array.pop(0)
+
+    prev_base = get_base(prev)
+    curr_base = get_base(curr)
+
+    #Extract zipped backups
+    prev_path = extract(prev, prev_base)
+    curr_path = extract(curr, curr_base)
+
+    #Migrate all CSVs
+    run_migrations(prev_path)
+    run_migrations(curr_path)
+
+    # Delete extracted directories
+    shutil.rmtree(prev_path)
+    shutil.rmtree(curr_path)
+
+    #Match Migrated CSVs
+    csv_array = sorted(os.listdir('temp'))
+    prev_array = [a for a in csv_array if prev_base in a]
+    curr_array = [a for a in csv_array if curr_base in a]
+
+    for i in range(len(prev_array)):
+        pdtype = prev_array[i].split('_')[1].split('.')[0]
+        schema = pdtype
+        if 'nil' in pdtype:
+            schema = schema.split('-')[0]
+        csv_diff(prev_array[i], curr_array[i],
+                 'http://open.canada.ca/data/en/recombinant-schema/{0}.json'.format(schema),
+                 '{0}_warehouse.csv'.format(pdtype))
+
+    shutil.rmtree('temp')
