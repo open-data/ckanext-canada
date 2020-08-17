b0d67992f3c3806563f57b7fdeeb0a3644cd3708
Ian Ward <ian@excess.org>
2020-08-17 15:25:15 -0400

    OPN-392: contracts country_of_origin → country_of_vendor

--- bin/migrate/migrate_contracts_2019_11.py
+++ bin/migrate/migrate_contracts_2019_11.py
@@ -9,7 +9,7 @@ from decimal import Decimal, InvalidOperation
 
 from openpyxl.utils.datetime import from_excel
 
-FIELDNAMES = 'reference_number,procurement_id,vendor_name,vendor_postal_code,buyer_name,contract_date,economic_object_code,description_en,description_fr,contract_period_start,delivery_date,contract_value,original_value,amendment_value,comments_en,comments_fr,additional_comments_en,additional_comments_fr,agreement_type_code,trade_agreement,land_claims,commodity_type,commodity_code,country_of_origin,solicitation_procedure,limited_tendering_reason,trade_agreement_exceptions,aboriginal_business,aboriginal_business_incidental,intellectual_property,potential_commercial_exploitation,former_public_servant,contracting_entity,standing_offer_number,instrument_type,ministers_office,number_of_bids,article_6_exceptions,award_criteria,socioeconomic_indicator,reporting_period,record_created,record_modified,user_modified,owner_org,owner_org_title'.split(',')
+FIELDNAMES = 'reference_number,procurement_id,vendor_name,vendor_postal_code,buyer_name,contract_date,economic_object_code,description_en,description_fr,contract_period_start,delivery_date,contract_value,original_value,amendment_value,comments_en,comments_fr,additional_comments_en,additional_comments_fr,agreement_type_code,trade_agreement,land_claims,commodity_type,commodity_code,country_of_vendor,solicitation_procedure,limited_tendering_reason,trade_agreement_exceptions,aboriginal_business,aboriginal_business_incidental,intellectual_property,potential_commercial_exploitation,former_public_servant,contracting_entity,standing_offer_number,instrument_type,ministers_office,number_of_bids,article_6_exceptions,award_criteria,socioeconomic_indicator,reporting_period,record_created,record_modified,user_modified,owner_org,owner_org_title'.split(',')
 
 assert sys.stdin.read(3) == codecs.BOM_UTF8
 sys.stdout.write(codecs.BOM_UTF8)
@@ -36,6 +36,7 @@ for line in in_csv:
     line['intellectual_property'] = line.pop('intellectual_property_code')
     line['contracting_entity'] = line.pop('standing_offer')
     line['instrument_type'] = line.pop('document_type_code')
+    line['country_of_vendor'] = line.pop('country_of_origin')
     line['number_of_bids'] = ''
     line['article_6_exceptions'] = ''
     line['award_criteria'] = ''
@@ -47,11 +48,11 @@ for line in in_csv:
         line['contracting_entity'] = 'PWSOSA'
     if line['contracting_entity'] in ('N/A', 'N', 'NUL'):
         line['contracting_entity'] = ''
-    line['country_of_origin'] = line['country_of_origin'].upper().strip()
-    if line['country_of_origin'].startswith('CAN'):
-        line['country_of_origin'] = 'CA'
-    if line['country_of_origin'].startswith('USA'):
-        line['country_of_origin'] = 'US'
+    line['country_of_vendor'] = line['country_of_vendor'].upper().strip()
+    if line['country_of_vendor'].startswith('CAN'):
+        line['country_of_vendor'] = 'CA'
+    if line['country_of_vendor'].startswith('USA'):
+        line['country_of_vendor'] = 'US'
     line['instrument_type'] = line['instrument_type'].upper().strip()
     line['intellectual_property'] = line['intellectual_property'].upper().strip()
     if ':' in line['intellectual_property']:
