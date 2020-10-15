52e6aebcbf75c50f20518ec99df236cc85feba6e
maxFredenburgh <maxwellfredenburgh.dev@gmail.com>
2020-10-15 14:25:44 -0400

    bash script automation fixes

--- bin/resource_management/url_database_script.sh
+++ bin/resource_management/url_database_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+dir=$1
+cd $dir
 wget http://open.canada.ca/static/od-do-canada.jl.gz
 gzip -d od-do-canada.jl.gz
 python url_database.py od-do-canada.jl 500 data/url_database_draft.csv
@@ -6,6 +8,6 @@ python url_database.py data/url_database_draft.csv 200 data/url_database.csv
 rm data/url_database_draft.csv
 python url_metadata_match.py od-do-canada.jl data/url_database.csv
 rm od-do-canada.jl
-the_date=$(date +"%y-%m-%d")
+the_date=$(date +"%Y-%m-%d")
 . recipients.config
-mail -s "Broken_Resource_Links-20$the_date.xlsx" -a reports/Broken_Resource_Links-20$the_date.xlsx $recipients < email_message.txt
+mail -s "Broken_Resource_Links-$the_date.xlsx" -a reports/Broken_Resource_Links-$the_date.xlsx $recipients < email_message.txt
