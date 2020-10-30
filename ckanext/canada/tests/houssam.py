a5cd7f8015e5514cc458b43e232809a5bb672f71
HoussamBedja <houssam.b@hotmail.com>
2020-10-30 09:32:52 -0400

    OPN-146: delete test file (pushed by mistake)

--- ckanext/canada/tests/houssam.py
+++
@@ -1,6 +0,0 @@
-import tempfile
-
-# f = tempfile.NamedTemporaryFile(suffix='xlsx')
-# file_path = tempfile.TemporaryDirectory() + '/empty_file.xlsx'
-f = tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx")
-print(f.name)
