7f8b27b0d6271d883724e0167591c71920f250f6
HoussamBedja <houssam.b@hotmail.com>
2021-01-12 13:11:16 -0500

    OPN-529: fix typo

--- bin/build-combined-ckan-mo.sh
+++ bin/build-combined-ckan-mo.sh
@@ -6,4 +6,4 @@ HERE=`dirname $0`
 cd "$HERE/.."
 python setup.py compile_catalog -f -d ../ckan/ckan/i18n --domain ckan --locale fr
 python setup.py compile_catalog -d ckanext/canada/i18n --domain ckanext-canada
-python setup.py compile_catalog -d ../ckanext-recombinant/ckanext/recombinant/i18n --domain ckanext-recombinan
+python setup.py compile_catalog -d ../ckanext-recombinant/ckanext/recombinant/i18n --domain ckanext-recombinant
