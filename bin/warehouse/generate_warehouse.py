762b27185ca354eb4b7e78c746e46fa8a9a70280
HoussamBedja <houssam.b@hotmail.com>
2021-01-31 18:45:03 -0500

    OPN-498: warehouse - use argparse and tempfile in the script

--- bin/warehouse/generate_warehouse.py
+++ bin/warehouse/generate_warehouse.py
@@ -11,21 +11,19 @@ import sys
 import os
 import subprocess
 import shutil
+import tempfile
 from datetime import datetime
+import argparse
 
-fname = sys.argv[1]
+parser = argparse.ArgumentParser(description="Run warehouse script. By default, it runs on the last 2 backups.")
+parser.add_argument("fname", help="directory of archived backups")
+parser.add_argument("-a", "--all", action='store_true', help="compare all backups.")
+args = parser.parse_args()
 
-if len(sys.argv) == 3:
-    operation = sys.argv[2]
-    if operation != '-a' and operation != '-d':
-        sys.exit("Error: invalid operation value (sys.argv[2]), -d or -a expected")
-else:
-    operation = "-d"
-
-tar_array = sorted(os.listdir(fname))
-if operation == "-d":
+tar_array = sorted(os.listdir(args.fname))
+if args.all == False:
     tar_array = tar_array[-2:]
-print(tar_array)
+
 prev = ''
 curr = ''
 
@@ -36,17 +34,16 @@ def get_base(tfile):
 
 def extract(tfile, dest):
     fpath = './' + dest
-    tar = tarfile.open(fname + tfile)
+    tar = tarfile.open(args.fname + tfile)
     tar.extractall(path=fpath)
     tar.close()
     return fpath
 
-def run_migrations(fpath):
-    if not os.path.exists('temp'):
-        os.mkdir('temp')
+def run_migrations(fpath, temp_dir):
+
     for csvfile in os.listdir(fpath):
         print("Migrating {0} from directory {1}".format(csvfile, fpath))
-        proc = subprocess.Popen(['python', 'migrate_all.py', fpath+'/'+csvfile, 'temp/'+fpath+'m_'+csvfile])
+        proc = subprocess.Popen(['python', 'migrate_all.py', fpath+'/'+csvfile, temp_dir+'/'+fpath+'m_'+csvfile])
         proc.wait()
 
 def csv_diff(prev_csv, curr_csv, endpoint, outfile):
@@ -54,7 +51,7 @@ def csv_diff(prev_csv, curr_csv, endpoint, outfile):
     dt_string = now.strftime("%Y-%m-%d")
 
     print("Getting difference between {0} and {1}".format(prev_csv, curr_csv))
-    proc = subprocess.Popen(['python', 'csv_diff.py', 'temp/'+prev_csv, 'temp/'+curr_csv, endpoint,
+    proc = subprocess.Popen(['python', 'csv_diff.py', temp_dir+'/'+prev_csv, temp_dir+'/'+curr_csv, endpoint,
                              dt_string, outfile])
     proc.wait()
 
@@ -63,45 +60,46 @@ if not os.path.exists('warehouse_reports'):
     os.mkdir('warehouse_reports')
 
 while tar_array:
-    if tar_array == []:
-        break
-    if prev == '':
-        prev = tar_array.pop(0)
-        curr = tar_array.pop(0)
-    else:
-        prev = curr
-        curr = tar_array.pop(0)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        if tar_array == []:
+            break
+        if prev == '':
+            prev = tar_array.pop(0)
+            curr = tar_array.pop(0)
+        else:
+            prev = curr
+            curr = tar_array.pop(0)
 
-    prev_base = get_base(prev)
-    curr_base = get_base(curr)
+        prev_base = get_base(prev)
+        curr_base = get_base(curr)
 
-    # Extract zipped backups
-    prev_path = extract(prev, prev_base)
-    curr_path = extract(curr, curr_base)
+        # Extract zipped backups
+        prev_path = extract(prev, prev_base)
+        curr_path = extract(curr, curr_base)
 
-    # Migrate all CSVs
-    run_migrations(prev_path)
-    run_migrations(curr_path)
+        # Migrate all CSVs
+        run_migrations(prev_path, temp_dir)
+        run_migrations(curr_path, temp_dir)
 
-    # Delete extracted directories
-    shutil.rmtree(prev_path)
-    shutil.rmtree(curr_path)
+        # Delete extracted directories
+        shutil.rmtree(prev_path)
+        shutil.rmtree(curr_path)
 
-    # Match Migrated CSVs
-    csv_array = sorted(os.listdir('temp'))
-    prev_array = [a for a in csv_array if prev_base in a]
-    curr_array = [a for a in csv_array if curr_base in a]
+        # Match Migrated CSVs
+        csv_array = sorted(os.listdir(temp_dir))
+        prev_array = [a for a in csv_array if prev_base in a]
+        curr_array = [a for a in csv_array if curr_base in a]
 
-    for i in range(len(prev_array)):
-        now = datetime.now()
-        dt_string = now.strftime("%H:%M:%s")
-        print(dt_string,'\n')
-        pdtype = prev_array[i].split('_')[1].split('.')[0]
-        schema = pdtype
-        if 'nil' in pdtype or 'std' in pdtype:
-            schema = schema.split('-')[0]
-        csv_diff(prev_array[i], curr_array[i],
-            'http://open.canada.ca/data/en/recombinant-schema/{0}.json'.format(schema),
-            'warehouse_reports/{0}_warehouse.csv'.format(pdtype))
+        for i in range(len(prev_array)):
+            now = datetime.now()
+            dt_string = now.strftime("%H:%M:%s")
+            print(dt_string,'\n')
+            pdtype = prev_array[i].split('_')[1].split('.')[0]
+            schema = pdtype
+            if 'nil' in pdtype or 'std' in pdtype:
+                schema = schema.split('-')[0]
+            csv_diff(prev_array[i], curr_array[i],
+                'http://open.canada.ca/data/en/recombinant-schema/{0}.json'.format(schema),
+                'warehouse_reports/{0}_warehouse.csv'.format(pdtype))
 
-    shutil.rmtree('temp')
+
