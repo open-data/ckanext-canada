32e197be6a61f7b7b8612820e981d9cd89e01e31
HoussamBedja <houssam.b@hotmail.com>
2021-02-04 08:50:21 -0500

    OPN-529: Fix & update ckan translation

--- bin/build-combined-ckan-mo.sh
+++ bin/build-combined-ckan-mo.sh
@@ -5,5 +5,6 @@ set -e
 HERE=`dirname $0`
 cd "$HERE/.."
 python setup.py compile_catalog -f -d ../ckan/ckan/i18n --domain ckan --locale fr
+mv ../ckan/ckan/i18n/fr/LC_MESSAGES/ckan.mo build/i18n/fr/LC_MESSAGES/
 python setup.py compile_catalog -d ckanext/canada/i18n --domain ckanext-canada
 python setup.py compile_catalog -d ../ckanext-recombinant/ckanext/recombinant/i18n --domain ckanext-recombinant
