283c6396b5df31ea0e8903598399e3b38cafe94f
Rabia Sajjad <rabiasajjad.dev@gmail.com>
2021-12-03 19:49:30 -0500

    minor code improvement for datastore_delete plugin

--- ckanext/canada/plugins.py
+++ ckanext/canada/plugins.py
@@ -718,24 +718,23 @@ def datastore_upsert(up_func, context, data_dict):
 @chained_action
 def datastore_delete(up_func, context, data_dict):
     if context.get('agent') == 'xloader':
-        result = up_func(context, data_dict)
+        return up_func(context, data_dict)
 
-    else:
-        lc = ckanapi.LocalCKAN(username=c.user)
-        res_id = data_dict['id'] if 'id' in data_dict.keys() else data_dict['resource_id']
-        res = lc.action.datastore_search(
-            resource_id=res_id,
-            filters=data_dict.get('filters'),
-            limit=1,
-        )
+    lc = ckanapi.LocalCKAN(username=c.user)
+    res_id = data_dict['id'] if 'id' in data_dict.keys() else data_dict['resource_id']
+    res = lc.action.datastore_search(
+        resource_id=res_id,
+        filters=data_dict.get('filters'),
+        limit=1,
+    )
 
-        result = up_func(context, data_dict)
+    result = up_func(context, data_dict)
 
-        act.datastore_activity_create(context,
-                                      {'count': res.get('total', 0),
-                                       'activity_type': 'deleted datastore',
-                                       'resource_id': res_id}
-                                      )
+    act.datastore_activity_create(context,
+                                  {'count': res.get('total', 0),
+                                   'activity_type': 'deleted datastore',
+                                   'resource_id': res_id}
+                                  )
     return result
 
 
