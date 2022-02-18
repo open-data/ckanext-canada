b4a778bbcd1e39a0f4024f31b88af9f20137f5e1
Rabia Sajjad <rabiasajjad.dev@gmail.com>
2022-02-18 18:05:18 -0500

    remove compile_catalog for local en for validation

--- bin/build-combined-ckan-mo.sh
+++ bin/build-combined-ckan-mo.sh
@@ -9,5 +9,4 @@ python setup.py compile_catalog -d ckanext/canada/i18n --domain ckanext-canada
 python setup.py compile_catalog -d ../ckanext-recombinant/ckanext/recombinant/i18n --domain ckanext-recombinant
 python setup.py compile_catalog --locale en -d ../goodtables/i18n/ -D goodtables
 python setup.py compile_catalog --locale fr -d ../goodtables/i18n/ -D goodtables
-python setup.py compile_catalog --locale en -d ../ckanext/validation/i18n -D ckanext-validation
 python setup.py compile_catalog --locale fr -d ../ckanext/validation/i18n -D ckanext-validation
