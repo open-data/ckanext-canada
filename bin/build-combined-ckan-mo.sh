ad23ed0a5fe6fe9ac93e06c5c53dec4fd1acc513
Rabia Sajjad <rabiasajjad.dev@gmail.com>
2022-02-18 13:56:35 -0500

    fix paths to i18n

--- bin/build-combined-ckan-mo.sh
+++ bin/build-combined-ckan-mo.sh
@@ -7,7 +7,7 @@ cd "$HERE/.."
 python setup.py compile_catalog -f -d ../ckan/ckan/i18n --domain ckan --locale fr
 python setup.py compile_catalog -d ckanext/canada/i18n --domain ckanext-canada
 python setup.py compile_catalog -d ../ckanext-recombinant/ckanext/recombinant/i18n --domain ckanext-recombinant
-python setup.py compile_catalog --locale en -d goodtables/i18n/ -D goodtables
-python setup.py compile_catalog --locale fr -d goodtables/i18n/ -D goodtables
-python setup.py compile_catalog --locale en -d ckanext/validation/i18n -D ckanext-validation
-python setup.py compile_catalog --locale fr -d ckanext/validation/i18n -D ckanext-validation
+python setup.py compile_catalog --locale en -d ../goodtables/i18n/ -D goodtables
+python setup.py compile_catalog --locale fr -d ../goodtables/i18n/ -D goodtables
+python setup.py compile_catalog --locale en -d ../ckanext/validation/i18n -D ckanext-validation
+python setup.py compile_catalog --locale fr -d ../ckanext/validation/i18n -D ckanext-validation
