f7f01eb517a87f1af864800644c6bb7d1aacd54e
Rabia Sajjad <rabiasajjad.dev@gmail.com>
2022-03-15 12:09:31 -0400

    update transitional orgs filters for org schema

--- bin/transitional_orgs_filter.py
+++ bin/transitional_orgs_filter.py
@@ -12,31 +12,22 @@ ckanapi dump organizations --all -r http://registry.data.gc.ca | bin/transitiona
 import sys
 import json
 
-EXTRAS = set([
-    'department_number',
-    'umd_number',
-    'shortform',
-    'shortform_fr',
-    'ati_email',
-    'opengov_email',
-    ])
+filtered_fields = {'id', 'name', 'title', 'title_translated',
+                   'department_number', 'umd_number', 'shortform',
+                   'ati_email', 'opengov_email'}
 
 users = '--users' in sys.argv
 
 for l in sys.stdin:
     o = json.loads(l)
-    old_extras = dict((e['key'], e['value']) for e in o['extras'])
-    line = {
-        "title": o["title"],
-        "id": o["id"],
-        "extras": [{"value": old_extras.get(ename, ''), "key": ename}
-            for ename in EXTRAS],
-        "name": o["name"],
-        }
+    line = {}
+    for key in o:
+        if key in filtered_fields:
+            line[key] = o[key]
+
     if users:
         line["users"] = [
             {"name": u["name"], "capacity": u["capacity"]}
             for u in o["users"]]
 
     print json.dumps(line)
-
