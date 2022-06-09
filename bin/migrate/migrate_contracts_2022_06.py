9866ccd7c291864f3a8752f698e2931566e4e09a
Ian Ward <ian@excess.org>
2022-06-09 16:38:30 -0400

    migration fixes

--- bin/migrate/migrate_contracts_2022_06.py
+++ bin/migrate/migrate_contracts_2022_06.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import sys
 import csv
@@ -8,8 +8,8 @@ assert sys.stdin.read(3) == codecs.BOM_UTF8
 
 reader = csv.DictReader(sys.stdin)
 writer = csv.DictWriter(sys.stdout, fieldnames=
-'reference_number,procurement_id,vendor_name,vendor_postal_code,buyer_name,contract_date,economic_object_code,description_en,description_fr,contract_period_start,delivery_date,contract_value,original_value,amendment_value,comments_en,comments_fr,additional_comments_en,additional_comments_fr,agreement_type_code,trade_agreement,land_claims,commodity_type,commodity_code,country_of_origin,solicitation_procedure,limited_tendering_reason,trade_agreement_exceptions,indigenous_business,indigenous_business_excluding_psib,intellectual_property,potential_commercial_exploitation,former_public_servant,contracting_entity,standing_offer_number,instrument_type,ministers_office,number_of_bids,article_6_exceptions,award_criteria,socioeconomic_indicator,reporting_period,record_created,record_modified,user_modified,owner_org,owner_org_title'.split(',')
-])
+'reference_number,procurement_id,vendor_name,vendor_postal_code,buyer_name,contract_date,economic_object_code,description_en,description_fr,contract_period_start,delivery_date,contract_value,original_value,amendment_value,comments_en,comments_fr,additional_comments_en,additional_comments_fr,agreement_type_code,trade_agreement,land_claims,commodity_type,commodity_code,country_of_vendor,solicitation_procedure,limited_tendering_reason,trade_agreement_exceptions,indigenous_business,indigenous_business_excluding_psib,intellectual_property,potential_commercial_exploitation,former_public_servant,contracting_entity,standing_offer_number,instrument_type,ministers_office,number_of_bids,article_6_exceptions,award_criteria,socioeconomic_indicator,reporting_period,record_created,record_modified,user_modified,owner_org,owner_org_title'.split(',')
+)
 writer.writeheader()
 
 try:
