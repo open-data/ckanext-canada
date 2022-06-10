6d9b090709d7d73f6e6483778a7cb888de30d87f
Ian Ward <ian@excess.org>
2022-06-10 10:33:00 -0400

    work with warehouse csvs too

--- bin/migrate/migrate_contracts_2022_06.py
+++ bin/migrate/migrate_contracts_2022_06.py
@@ -6,16 +6,23 @@ import codecs
 
 assert sys.stdin.read(3) == codecs.BOM_UTF8
 
+RENAME_COLUMNS = {
+    'aboriginal_business': 'indigenous_business',
+    'aboriginal_business_incidental': 'indigenous_business_excluding_psib',
+}
+
 reader = csv.DictReader(sys.stdin)
-writer = csv.DictWriter(sys.stdout, fieldnames=
-'reference_number,procurement_id,vendor_name,vendor_postal_code,buyer_name,contract_date,economic_object_code,description_en,description_fr,contract_period_start,delivery_date,contract_value,original_value,amendment_value,comments_en,comments_fr,additional_comments_en,additional_comments_fr,agreement_type_code,trade_agreement,land_claims,commodity_type,commodity_code,country_of_vendor,solicitation_procedure,limited_tendering_reason,trade_agreement_exceptions,indigenous_business,indigenous_business_excluding_psib,intellectual_property,potential_commercial_exploitation,former_public_servant,contracting_entity,standing_offer_number,instrument_type,ministers_office,number_of_bids,article_6_exceptions,award_criteria,socioeconomic_indicator,reporting_period,record_created,record_modified,user_modified,owner_org,owner_org_title'.split(',')
+writer = csv.DictWriter(
+    sys.stdout,
+    fieldnames=list(
+        RENAME_COLUMNS.get(name, name) for name in reader.fieldnames),
 )
 writer.writeheader()
 
 try:
     for row in reader:
-        row['indigenous_business'] = row.pop('aboriginal_business')
-        row['indigenous_business_excluding_psib'] = row.pop('aboriginal_business_incidental')
+        for old, new in RENAME_COLUMNS.items():
+            row[new] = row.pop(old)
         writer.writerow(row)
 
 except KeyError:
