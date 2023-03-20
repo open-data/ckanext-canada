798aaeb9b0d09df33c3be63d0d02d17d2c30f56b
JVickery-TBS <jesse.vickery@tbs-sct.gc.ca>
2023-03-20 17:39:50 +0000

    feat(templates): cont template merge;
    - Removed template directory from internal plugin update config.
    - Merged missing files from public into root template dir.

--- ckanext/canada/plugins.py
+++ ckanext/canada/plugins.py
@@ -198,7 +198,6 @@ class DataGCCAInternal(p.SingletonPlugin):
 
     # IConfigurer
     def update_config(self, config):
-        p.toolkit.add_template_directory(config, 'templates')
         p.toolkit.add_public_directory(config, 'internal/static')
 
         config.update({
