b9a60771d143eddaf5f4a67d8eb2fae1de167bdd
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-04-03 19:23:59 +0000

    feat(dev): finalized tabulator classes;
    - Finalized tabulator classes.
    - Finalized dialect checks.

--- ckanext/canada/checks.py
+++ ckanext/canada/checks.py
@@ -1,5 +1,6 @@
 from copy import copy
 from goodtables import check, Error
+from tabulator.exceptions import TabulatorException
 import json
 
 from ckan.plugins.toolkit import _, config
@@ -41,21 +42,27 @@ def ds_headers_check(cells, sample=None):
 @check('static-dialect', type='custom')
 class StaticDialectCheck(object):
 
-    validation_options = None
+    static_dialects = None
     static_dialect = None
     stream_dialect = None
+    errors = None
 
     def __init__(self, **options):
-        static_validation_options = config.get(
-            u'ckanext.validation.static_validation_options')
-        if static_validation_options:
-            self.validation_options = json.loads(static_validation_options)
+        static_dialects = config.get(
+            u'ckanext.canada.tabulator_dialects')
+        if static_dialects:
+            self.static_dialects = json.loads(static_dialects)
 
     def prepare(self, stream, schema, extra):
-        if self.validation_options:
-            self.static_dialect = self.validation_options.get('dialect', {}) \
-                .get(stream.format, None)
-        self.stream_dialect = stream.dialect
+        if self.static_dialects:
+            # need to retrieve static dialect here as we have the format.
+            self.static_dialect = self.static_dialects.get(stream.format, None)
+        try:
+            self.stream_dialect = stream.dialect
+        except TabulatorException as e:
+            self.errors = e.message
+            pass
+
         # always return True so we use this check
         return True
 
@@ -70,22 +77,8 @@ class StaticDialectCheck(object):
 
         errors = []
 
-        if 'delimiter' in self.stream_dialect and self.stream_dialect['delimiter'] != self.static_dialect['delimiter']:
-            errors.append(Error('invalid-dialect',
-                                message=_("File is using delimeter {stream_delimeter} instead of {static_delimeter}"),
-                                message_substitutions={'stream_delimeter': self.stream_dialect['delimiter'],
-                                                       'static_delimeter': self.static_dialect['delimiter'],}))
-
-        if 'quoteChar' in self.stream_dialect and self.stream_dialect['quoteChar'] != self.static_dialect['quotechar']:
-            errors.append(Error('invalid-quote-char',
-                                message=_("File is using quoting character {stream_quote_char} instead of {static_quote_char}"),
-                                message_substitutions={'stream_quote_char': self.stream_dialect['quoteChar'],
-                                                       'static_quote_char': self.static_dialect['quotechar'],}))
-
-        if 'doubleQuote' in self.stream_dialect and self.stream_dialect['doubleQuote'] != self.static_dialect['doublequote']:
-            errors.append(Error('invalid-double-quote',
-                                message=_("File is using double quoting {stream_double_quote} instead of {static_double_quote}"),
-                                message_substitutions={'stream_double_quote': self.stream_dialect['doubleQuote'],
-                                                       'static_double_quote': self.static_dialect['doublequote'],}))
+        if self.errors and isinstance(self.errors, list):
+            for err_id, err_msg in self.errors:
+                errors.append(Error(err_id, message=err_msg))
 
         return errors, len(errors) > 0
--- ckanext/canada/tabulate.py
+++ ckanext/canada/tabulate.py
@@ -11,12 +11,12 @@ Note: script cannot be named tabulator! (hence tabulate)
 from tabulator.stream import Stream
 from tabulator.parsers.csv import CSVParser
 from tabulator.config import CSV_SAMPLE_LINES
-from tabulator.exceptions import TabulatorException
+from tabulator.exceptions import TabulatorException, EncodingError
 import six
 import json
+import csv
 from csv import Dialect
 from _csv import Dialect as _Dialect
-from csv import QUOTE_MINIMAL
 
 from ckan.plugins.toolkit import config, _
 
@@ -36,6 +36,16 @@ class LimitStreamLogging(Filter):
 log = getLogger(__name__)
 log.addFilter(LimitStreamLogging())
 
+# Xloader uses a universal decoder to pre-fligh guess
+# the encoding of a file. We should allow some of the
+# unicode type encoding sets/subsets.
+ENCODING_SUBSETS = [
+    'UTF-8-SIG',
+    'UTF-8',
+    'ascii',
+    'ASCII',
+]
+
 
 class CanadaStream(Stream):
     """
@@ -50,18 +60,26 @@ class CanadaStream(Stream):
 
     static_dialects = None
     static_encoding = None
-    encoding = None
 
     def __init__(self, source, *args, **kwargs):
         kwargs.update({'custom_parsers': {'csv': CanadaCSVParser}})
         encoding = _get_encoding()
         if encoding:
-            kwargs.update({'encoding': encoding})
             self.static_encoding = encoding
         super(CanadaStream, self).__init__(source, *args, **kwargs)
         dialects = _get_dialects()
         if dialects:
             self.static_dialects = dialects
+
+    @property
+    def encoding(self):
+        if self.static_encoding and super(CanadaStream, self).encoding != 'no':
+            # if the stream does not have a guessed encoding yet, it will be set to 'no'.
+            # we only want to compare encoding once the stream has guessed it.
+            _check_encoding_and_raise(super(CanadaStream, self).encoding, self.static_encoding)
+            return self.static_encoding
+
+        return super(CanadaStream, self).encoding
 
     @property
     def dialect(self):
@@ -75,31 +93,8 @@ class CanadaStream(Stream):
             if self.static_encoding:
                 log.debug('Using static encoding for %s: %s', self.format, self.static_encoding)
             log.debug('Using Static Dialect for %s: %r', self.format, self.static_dialects[self.format])
-            return self.static_dialects[self.format]
-
-        return super(CanadaStream, self).dialect
-
-
-class CanadaStreamWithRaises(CanadaStream):
-    """
-    Same as CanadaStream, but will raise TabulatorException(s)
-
-    Should be used when needed to raise exceptions to stop Tabulator during streaming.
-    """
-    @property
-    def encoding(self):
-        if self.static_encoding:
-            _check_encoding_and_raise(super(CanadaStream, self).encoding, self.static_encoding)
-
-        return super(CanadaStreamWithRaises, self).encoding
-
-    @property
-    def dialect(self):
-        if self.static_dialects and self.format in self.static_dialects:
-            if self.static_encoding:
-                log.debug('Using static encoding for %s: %s', self.format, self.static_encoding)
-            log.debug('Using Static Dialect for %s: %r', self.format, self.static_dialects[self.format])
             _check_dialect_and_raise(super(CanadaStream, self).dialect, self.static_dialects[self.format])
+            return self.static_dialects[self.format]
 
         return super(CanadaStream, self).dialect
 
@@ -122,7 +117,7 @@ class CanadaCSVDialect(Dialect):
     doublequote = True
     skipinitialspace = False
     lineterminator = "\r\n"
-    quoting = QUOTE_MINIMAL
+    quoting = csv.QUOTE_MINIMAL
 
     def __init__(self, static_dialect):
         """
@@ -158,19 +153,16 @@ class CanadaCSVParser(CSVParser):
 
     static_dialects = None
     static_encoding = None
-    encoding = None
 
     # custom options, these need to exist for some magic.
     options = [
         'static_dialect',
         'static_encoding',
-        'encoding',
     ]
 
     def __init__(self, loader, *args, **kwargs):
         encoding = _get_encoding()
         if encoding:
-            kwargs.update({'encoding': encoding})
             self.static_encoding = encoding
         super(CanadaCSVParser, self).__init__(loader, *args, **kwargs)
         dialects = _get_dialects()
@@ -181,11 +173,22 @@ class CanadaCSVParser(CSVParser):
             self._CSVParser__prepare_dialect = self.__mangle__prepare_dialect
 
     @property
+    def encoding(self):
+        if self.static_encoding and super(CanadaCSVParser, self).encoding != 'no':
+            # if the stream does not have a guessed encoding yet, it will be set to 'no'.
+            # we only want to compare encoding once the stream has guessed it.
+            _check_encoding_and_raise(super(CanadaCSVParser, self).encoding, self.static_encoding)
+            return self.static_encoding
+
+        return super(CanadaCSVParser, self).encoding
+
+    @property
     def dialect(self):
         if self.static_dialects and 'csv' in self.static_dialects:
             if self.static_encoding:
                 log.debug('Using static encoding for csv: %s', self.static_encoding)
             log.debug('Using Static Dialect for csv: %r', self.static_dialects['csv'])
+            _check_dialect_and_raise(super(CanadaCSVParser, self).dialect, self.static_dialects['csv'])
             return self.static_dialects['csv']
 
         return super(CanadaCSVParser, self).dialect
@@ -203,9 +206,33 @@ class CanadaCSVParser(CSVParser):
             if len(sample) >= CSV_SAMPLE_LINES:
                 break
 
+        # Get dialect
+        # Copied from tabulator.pasrers.csv
+        # Needed to get the guessed dialect. Using mangling for protected properties.
+        try:
+            separator = b'' if six.PY2 else ''
+            delimiter = self._CSVParser__options.get('delimiter', ',\t;|')
+            dialect = csv.Sniffer().sniff(separator.join(sample), delimiter)
+            if not dialect.escapechar:
+                dialect.doublequote = True
+        except csv.Error:
+            class dialect(csv.excel):
+                pass
+        for key, value in self._CSVParser__options.items():
+            setattr(dialect, key, value)
+        # https://github.com/frictionlessdata/FrictionlessDarwinCore/issues/1
+        if getattr(dialect, 'quotechar', None) == '':
+            setattr(dialect, 'quoting', csv.QUOTE_NONE)
+
+        self._CSVParser__dialect = dialect
+
         if self.static_encoding:
             log.debug('Using static encoding for csv: %s', self.static_encoding)
         log.debug('Using Static Dialect for csv: %r', self.static_dialects['csv'])
+
+        # Xloader does not use Goodtables, and enters the Stream Parser fairly directly.
+        # So we need to check and raise here. self.static_dialects will always exist in this mangle.
+        _check_dialect_and_raise(self._CSVParser__dialect, CanadaCSVDialect(self.static_dialects['csv']), compare_obj=True)
 
         return sample, CanadaCSVDialect(self.static_dialects['csv'])
 
@@ -221,24 +248,35 @@ def _get_encoding():
     return config.get('ckanext.canada.tabulator_encoding', None)
 
 
-def _check_dialect_and_raise(guessed_dialect, static_dialect):
+def _check_dialect_and_raise(guessed_dialect, static_dialect, compare_obj=False):
+    errors = []
+
+    if compare_obj:
+        # a bit nasty, but need to compare from __mangle__prepare_dialect
+        guessed_dialect = guessed_dialect.__dict__
+        static_dialect = static_dialect.__dict__
+
     if guessed_dialect and static_dialect:
         if 'delimiter' in guessed_dialect and guessed_dialect['delimiter'] != static_dialect['delimiter']:
-            raise TabulatorException(_("File is using delimeter {stream_delimeter} instead of {static_delimeter}").format(
-                stream_delimeter=guessed_dialect['delimiter'],
-                static_delimeter=static_dialect['delimiter']))
+            errors.append(('invalid-dialect', _("File is using delimeter {stream_delimeter} instead of {static_delimeter}").format(
+                                                stream_delimeter=guessed_dialect['delimiter'],
+                                                static_delimeter=static_dialect['delimiter'])))
 
         if 'quoteChar' in guessed_dialect and guessed_dialect['quoteChar'] != static_dialect['quotechar']:
-            raise TabulatorException(_("File is using quoting character {stream_quote_char} instead of {static_quote_char}").format(
-                stream_quote_char=guessed_dialect['quoteChar'],
-                static_quote_char=static_dialect['quotechar']))
+            errors.append(('invalid-quote-char', _("File is using quoting character {stream_quote_char} instead of {static_quote_char}").format(
+                                                   stream_quote_char=guessed_dialect['quoteChar'],
+                                                   static_quote_char=static_dialect['quotechar'])))
 
         if 'doubleQuote' in guessed_dialect and guessed_dialect['doubleQuote'] != static_dialect['doublequote']:
-            raise TabulatorException(_("File is using double quoting {stream_double_quote} instead of {static_double_quote}").format(
-                stream_double_quote=guessed_dialect['doubleQuote'],
-                static_double_quote=static_dialect['doublequote']))
+            errors.append(('invalid-double-quote', _("File is using double quoting {stream_double_quote} instead of {static_double_quote}").format(
+                                                     stream_double_quote=guessed_dialect['doubleQuote'],
+                                                     static_double_quote=static_dialect['doublequote'])))
+
+    if errors:
+        raise TabulatorException(errors)
 
 
 def _check_encoding_and_raise(guessed_encoding, static_encoding):
-    if guessed_encoding != static_encoding:
-        raise TabulatorException(_('File must be encoded with: %s' % static_encoding))
+    if guessed_encoding in ENCODING_SUBSETS or guessed_encoding == static_encoding:
+        return
+    raise EncodingError(_('File must be encoded with: %s' % static_encoding))
