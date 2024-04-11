8ffbff5a957d84463378467a635efb2b6334c40d
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-04-11 15:38:08 +0000

    feat(tests): make test coverage;
    - Added all pd type test coverage for make.

--- ckanext/canada/tests/test_make.py
+++ ckanext/canada/tests/test_make.py
@@ -22,12 +22,23 @@ PD_FILTER_SCRIPT_DIRECTORY = '{0}/{1}'.format(os.path.dirname(os.path.realpath(_
 
 @pytest.fixture()
 def set_ini_class_prop(request):
-    #TODO: get abspath of the --ckan-ini
-    setattr(request.cls, 'ckan_ini', request.config.getoption('--ckan-ini'))
+    ckan_ini = request.config.getoption('--ckan-ini')
+    if not ckan_ini.startswith('/'):
+        ckan_ini = '{0}/{1}'.format(os.getcwd(), ckan_ini)
+    setattr(request.cls, 'ckan_ini', ckan_ini)
 
 
 @pytest.mark.usefixtures("set_ini_class_prop")
 class TestMakePD(CanadaTestBase):
+    """
+    Test cases for nightly re-builds of PD types.
+
+    These are extremely important test cases to ensure that PD records
+    are re-built and published properly.
+
+    # TODO: update Django search re-build test cases once we have a
+    #       published Docker image of our search application.
+    """
     @classmethod
     def setup_method(self, method):
         """Method is called at class level before EACH test methods of the class are called.
@@ -35,7 +46,8 @@ class TestMakePD(CanadaTestBase):
         """
         super(TestMakePD, self).setup_method(method)
 
-        self.org = Organization()
+        self.org = Organization(umd_number='example_umd',
+                                department_number='example_department')
 
         self.action = LocalCKAN().action
 
@@ -61,16 +73,17 @@ class TestMakePD(CanadaTestBase):
 
 
     def _setup_ini(self, ini):
+        assert ini.startswith('/')
         os.environ['REGISTRY_INI'] = ini
         os.environ['PORTAL_INI'] = ini
 
 
-    def _setup_pd(self, type, nil_type):
+    def _setup_pd(self, type, nil_type=None, extra_resource_ids=[]):
         assert type
 
         self.action.recombinant_create(dataset_type=type, owner_org=self.org['name'])
 
-        rval = self.action.recombinant_show(dataset_type='ati', owner_org=self.org['name'])
+        rval = self.action.recombinant_show(dataset_type=type, owner_org=self.org['name'])
 
         chromo = get_chromo(type)
 
@@ -78,16 +91,21 @@ class TestMakePD(CanadaTestBase):
             resource_id=rval['resources'][0]['id'],
             records=[chromo['examples']['record']])
 
-        Resource(id=chromo['published_resource_id'])
+        if 'published_resource_id' in chromo:
+            Resource(id=chromo['published_resource_id'])
 
         if nil_type:
-            nil_chromo = get_chromo(type)
+            nil_chromo = get_chromo(nil_type)
 
             self.action.datastore_upsert(
                 resource_id=rval['resources'][1]['id'],
                 records=[nil_chromo['examples']['record']])
 
-            Resource(id=nil_chromo['published_resource_id'])
+            if 'published_resource_id' in nil_chromo:
+                Resource(id=nil_chromo['published_resource_id'])
+
+        for _id in extra_resource_ids:
+            Resource(id=_id)
 
 
     def test_enivonment_variables(self):
@@ -119,4 +137,346 @@ class TestMakePD(CanadaTestBase):
         make_process = subprocess.Popen(["make upload-ati"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         stdout, stderr = make_process.communicate()
 
-        #TODO: assert any failures
+        assert "Usage:" not in stdout
+        assert "upload-ati] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-ati"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Drupal/Solr, test for record indexing
+        assert "Usage:" not in stdout
+        assert "rebuild-ati] Error" not in stdout
+        assert '%s 1' % self.org['name'] in stdout
+
+
+    def test_make_briefingt(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='briefingt', nil_type='briefingt-nil')
+
+        make_process = subprocess.Popen(["make upload-briefingt"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-briefingt] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-briefingt"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-briefingt] Error" not in stdout
+        assert 'import_data_csv' in stdout
+
+
+    def test_make_qpnotes(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='qpnotes', nil_type='qpnotes-nil')
+
+        make_process = subprocess.Popen(["make upload-qpnotes"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-qpnotes] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-qpnotes"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-qpnotes] Error" not in stdout
+        assert 'import_data_csv' in stdout
+
+
+    def test_make_contracts(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='contracts', nil_type='contracts-nil')
+
+        make_process = subprocess.Popen(["make upload-contracts"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-contracts] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-contracts"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-contracts] Error" not in stdout
+        assert 'import_data_csv' in stdout
+
+
+    def test_make_contractsa(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='contractsa')
+
+        make_process = subprocess.Popen(["make upload-contractsa"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-contractsa] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-contractsa"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # there is no search for contractsa, just test for no errors
+        assert "Usage:" not in stdout
+        assert "rebuild-contractsa] Error" not in stdout
+
+
+    def test_make_consultations(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='consultations',
+                       extra_resource_ids=['897d3008-b258-4a68-8c02-3e3c099a42c8',
+                                           'aa606d25-4387-4bc6-89eb-12d66f5e9044',])
+
+        make_process = subprocess.Popen(["make upload-consultations"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-consultations] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-consultations"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # there is no search for contractsa, just test for no errors
+        assert "Usage:" not in stdout
+        assert "rebuild-consultations] Error" not in stdout
+
+
+    def test_make_dac(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='dac')
+
+        make_process = subprocess.Popen(["make upload-dac"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-dac] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-dac"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # there is no search for contractsa, just test for no errors
+        assert "Usage:" not in stdout
+        assert "rebuild-dac] Error" not in stdout
+
+
+    def test_make_experiment(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='experiment')
+
+        make_process = subprocess.Popen(["make upload-experiment"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-experiment] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-experiment"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # there is no search for contractsa, just test for no errors
+        assert "Usage:" not in stdout
+        assert "rebuild-experiment] Error" not in stdout
+
+
+    def test_make_grants(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='grants', nil_type='grants-nil')
+
+        make_process = subprocess.Popen(["make upload-grants"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-grants] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-grants"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-grants] Error" not in stdout
+        assert 'import_data_csv' in stdout
+
+
+    def test_make_hospitalityq(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='hospitalityq', nil_type='hospitalityq-nil')
+
+        make_process = subprocess.Popen(["make upload-hospitalityq"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-hospitalityq] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-hospitalityq"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Drupal/Solr, test for record indexing
+        assert "Usage:" not in stdout
+        assert "rebuild-hospitalityq] Error" not in stdout
+        assert '%s 1' % self.org['name'] in stdout
+
+
+    def test_make_travelq(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='travelq', nil_type='travelq-nil')
+
+        make_process = subprocess.Popen(["make upload-travelq"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-travelq] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-travelq"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-travelq] Error" not in stdout
+        assert 'import_data_csv' in stdout
+
+
+    def test_make_nap5(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='nap5')
+
+        make_process = subprocess.Popen(["make upload-nap5"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-nap5] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-nap5"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-nap5] Error" not in stdout
+        assert 'import_data_csv' in stdout
+
+
+    def test_make_reclassification(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='reclassification', nil_type='reclassification-nil')
+
+        make_process = subprocess.Popen(["make upload-reclassification"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-reclassification] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-reclassification"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Drupal/Solr, test for record indexing
+        assert "Usage:" not in stdout
+        assert "rebuild-reclassification] Error" not in stdout
+        assert '%s 1' % self.org['name'] in stdout
+
+
+    def test_make_service(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='service', nil_type='service-std')
+
+        make_process = subprocess.Popen(["make upload-service"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-service] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-service"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # there is no search for contractsa, just test for no errors
+        assert "Usage:" not in stdout
+        assert "rebuild-service] Error" not in stdout
+
+
+    def test_make_travela(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='travela')
+
+        make_process = subprocess.Popen(["make upload-travela"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-travela] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-travela"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-travela] Error" not in stdout
+        assert 'import_data_csv' in stdout
+
+
+    def test_make_wrongdoing(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='wrongdoing')
+
+        make_process = subprocess.Popen(["make upload-wrongdoing"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-wrongdoing] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-wrongdoing"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Drupal/Solr, test for record indexing
+        assert "Usage:" not in stdout
+        assert "rebuild-wrongdoing] Error" not in stdout
+        assert '%s 1' % self.org['name'] in stdout
+
+
+    def test_make_adminaircraft(self):
+        assert self.ckan_ini
+        self._setup_ini(self.ckan_ini)
+
+        self._setup_pd(type='adminaircraft')
+
+        make_process = subprocess.Popen(["make upload-adminaircraft"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        assert "Usage:" not in stdout
+        assert "upload-adminaircraft] Error" not in stdout
+
+        make_process = subprocess.Popen(["make rebuild-adminaircraft"], shell=True, cwd=MAKE_PATH, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout, stderr = make_process.communicate()
+
+        # Django, just test for command output from echo
+        assert "Usage:" not in stdout
+        assert "rebuild-adminaircraft] Error" not in stdout
+        assert 'import_data_csv' in stdout
