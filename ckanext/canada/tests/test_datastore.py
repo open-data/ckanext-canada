18fe2860947a6731069b2a915bb649c9b0915561
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-05-10 17:06:40 +0000

    feat(tests): datastore tests;
    - Continued new datastore tests.

--- ckanext/canada/tests/test_datastore.py
+++ ckanext/canada/tests/test_datastore.py
@@ -1,33 +1,110 @@
 # -*- coding: UTF-8 -*-
 import logging
+import mock
+from cgi import FieldStorage
 from ckanext.canada.tests import CanadaTestBase
 from ckan import plugins
+from ckan.lib.uploader import ResourceUpload
+from ckan.tests.helpers import change_config
 
 import pytest
 from ckanext.canada.tests.factories import (
     CanadaResource as Resource,
-    get_sample_filepath
+    MockFieldStorage,
+    get_sample_filepath,
+    mock_uploads,
 )
 from ckanext.xloader import loader
 from ckanext.xloader.job_exceptions import LoaderError
+from ckanext.validation.jobs import run_validation_job
 
+from ckanapi import LocalCKAN
 
 logger = logging.getLogger(__name__)
 
 
-class TestDatastoreStuff(CanadaTestBase):
+class MockUploader(ResourceUpload):
+    def get_path(self, resource_id):
+        return '/tmp/example/{}'.format(resource_id)
+
+
+def mock_get_resource_uploader(data_dict):
+    return MockUploader(data_dict)
+
+
+class TestDatastoreValidation(CanadaTestBase):
     @classmethod
     def setup_method(self, method):
         """Method is called at class level before EACH test methods of the class are called.
         Setup any state specific to the execution of the given class methods.
         """
-        super(TestDatastoreStuff, self).setup_method(method)
+        super(TestDatastoreValidation, self).setup_method(method)
+
+        if plugins.plugin_loaded('xloader'):
+            plugins.unload('xloader')
+
+        if plugins.plugin_loaded('cloudstorage'):
+            plugins.unload('cloudstorage')
+
+        if not plugins.plugin_loaded('validation'):
+            plugins.load('validation')
+
+        self.action = LocalCKAN().action
+
+
+    @classmethod
+    def teardown_method(self):
+        """Method is called at class level after EACH test methods of the class are called.
+        Close any state specific to the execution of the given class methods.
+        """
+        if plugins.plugin_loaded('xloader'):
+            plugins.unload('xloader')
+
+        if plugins.plugin_loaded('cloudstorage'):
+            plugins.unload('cloudstorage')
+
+        if not plugins.plugin_loaded('validation'):
+            plugins.load('validation')
+
+
+    @change_config('ckanext.validation.run_on_create_async', False)
+    @change_config('ckanext.validation.run_on_update_async', False)
+    @mock_uploads
+    @mock.patch('ckanext.validation.jobs.get_resource_uploader', mock_get_resource_uploader)
+    def test_validation_report(self, mock_open):
+        csv_filepath = get_sample_filepath("sample.csv")
+
+        with open(csv_filepath, 'r') as f:
+            resource = Resource(url='__upload',
+                                url_type='upload',
+                                format='CSV',
+                                upload=MockFieldStorage(f, 'sample.csv'))
+
+        run_validation_job(resource)
+
+        report = self.action.resource_validation_show(resource_id=resource.get('id'))
+
+        logger.info("    ")
+        logger.info("DEBUGGING::")
+        logger.info("    ")
+        logger.info(report)
+        logger.info("    ")
+        assert False
+
+
+class TestDatastoreXloader(CanadaTestBase):
+    @classmethod
+    def setup_method(self, method):
+        """Method is called at class level before EACH test methods of the class are called.
+        Setup any state specific to the execution of the given class methods.
+        """
+        super(TestDatastoreXloader, self).setup_method(method)
 
         if not plugins.plugin_loaded('xloader'):
             plugins.load('xloader')
 
-        if not plugins.plugin_loaded('validation'):
-            plugins.load('validation')
+        if plugins.plugin_loaded('validation'):
+            plugins.unload('validation')
 
         self.action = LocalCKAN().action
 
@@ -43,8 +120,8 @@ class TestDatastoreStuff(CanadaTestBase):
         if plugins.plugin_loaded('xloader'):
             plugins.unload('xloader')
 
-        if plugins.plugin_loaded('validation'):
-            plugins.unload('validation')
+        if not plugins.plugin_loaded('validation'):
+            plugins.load('validation')
 
 
     def _get_ds_records(self):
@@ -52,7 +129,7 @@ class TestDatastoreStuff(CanadaTestBase):
         return result.get('fields'), result.get('records')
 
 
-    def test_xloader_load_csv(self):
+    def test_load_csv(self):
         csv_filepath = get_sample_filepath("sample.csv")
 
         loader.load_csv(
@@ -84,7 +161,7 @@ class TestDatastoreStuff(CanadaTestBase):
         assert records == expected_records
 
 
-    def test_xloader_load_table(self):
+    def test_load_table(self):
         csv_filepath = get_sample_filepath("sample.csv")
 
         loader.load_table(
@@ -116,7 +193,7 @@ class TestDatastoreStuff(CanadaTestBase):
         assert records == expected_records
 
 
-    def test_xloader_load_csv_bad_ds_headers(self):
+    def test_load_csv_bad_ds_headers(self):
         csv_filepath = get_sample_filepath("sample_with_bad_ds_headers.csv")
 
         with pytest.raises(LoaderError) as le:
@@ -130,7 +207,7 @@ class TestDatastoreStuff(CanadaTestBase):
         assert '"_thisisnotallowed" is not a valid field name' in str(le)
 
 
-    def test_xloader_load_table_bad_ds_headers(self):
+    def test_load_table_bad_ds_headers(self):
         csv_filepath = get_sample_filepath("sample_with_bad_ds_headers.csv")
 
         with pytest.raises(LoaderError) as le:
@@ -144,7 +221,7 @@ class TestDatastoreStuff(CanadaTestBase):
         assert '"_thisisnotallowed" is not a valid field name' in str(le)
 
 
-    def test_xloader_load_csv_empty_lines(self):
+    def test_load_csv_empty_lines(self):
         csv_filepath = get_sample_filepath("sample_with_empty_lines.csv")
 
         loader.load_csv(
@@ -176,7 +253,7 @@ class TestDatastoreStuff(CanadaTestBase):
         assert records == expected_records
 
 
-    def test_xloader_load_table_empty_lines(self):
+    def test_load_table_empty_lines(self):
         csv_filepath = get_sample_filepath("sample_with_empty_lines.csv")
 
         loader.load_table(
@@ -208,7 +285,7 @@ class TestDatastoreStuff(CanadaTestBase):
         assert records == expected_records
 
 
-    def test_xloader_load_csv_white_space(self):
+    def test_load_csv_white_space(self):
         csv_filepath = get_sample_filepath("sample_with_extra_white_space.csv")
 
         loader.load_csv(
@@ -247,7 +324,7 @@ class TestDatastoreStuff(CanadaTestBase):
         assert records == expected_records
 
 
-    def test_xloader_load_table_white_space(self):
+    def test_load_table_white_space(self):
         csv_filepath = get_sample_filepath("sample_with_extra_white_space.csv")
 
         loader.load_table(
@@ -277,10 +354,3 @@ class TestDatastoreStuff(CanadaTestBase):
 
         assert fields == expected_fields
         assert records == expected_records
-
-        # logger.info("    ")
-        # logger.info("DEBUGGING::")
-        # logger.info("    ")
-        # logger.info(le.message)
-        # logger.info("    ")
-        # assert False
