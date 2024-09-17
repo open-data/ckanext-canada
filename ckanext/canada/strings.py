6170e74c295340d99ab5f80735a3f738dc213dcd
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-09-17 18:14:35 +0000

    feat(pd): services 2024;
    - i18n.

--- ckanext/canada/strings.py
+++ ckanext/canada/strings.py
@@ -61,6 +61,8 @@ _('Promote user to Sysadmin')
 _('Promote')
 _('Cannot modify your own sysadmin privileges')
 _('Cannot modify sysadmin privileges for system user')
+_('Show more')
+_('Hide')
 
 # strings from security
 _("Please upload a file or link to an external resource")
