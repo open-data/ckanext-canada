8c6dd025214822888c1d7fa44bc92ce7b2cd82de
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2024-12-17 18:38:45 +0000

    feat(dev): linting;
    - Started linting.

--- bin/filter/filter_adminaircraft.py
+++ bin/filter/filter_adminaircraft.py
@@ -12,6 +12,7 @@ REMOVE_COLUMNS = [
     'owner_org_title'
 ]
 
+
 def main():
     reader = csv.DictReader(sys.stdin)
     outnames = [f for f in reader.fieldnames if f not in REMOVE_COLUMNS]
@@ -24,5 +25,6 @@ def main():
             writer.writerow(row)
         except ValueError:
             pass
+
 
 main()
