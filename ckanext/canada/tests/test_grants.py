a5b07112bccb88a25343d02a40e254c1cdd6a3ea
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-03-05 14:45:51 +0000

    fix(pd): grantsmonthly column type;
    - `text` -> `_text`
    - Added test class for grants monthly.

--- ckanext/canada/tests/test_grants.py
+++ ckanext/canada/tests/test_grants.py
@@ -46,3 +46,35 @@ class TestGrants(CanadaTestBase):
         self.lc.action.datastore_upsert(
             resource_id=self.resource_id,
             records=[record])
+
+# (staging fork only): grants monthly
+class TestGrantsMonthly(CanadaTestBase):
+    @classmethod
+    def setup_method(self, method):
+        """Method is called at class level before EACH test methods of the class are called.
+        Setup any state specific to the execution of the given class methods.
+        """
+        super(TestGrantsMonthly, self).setup_method(method)
+
+        org = Organization()
+        self.lc = LocalCKAN()
+
+        self.lc.action.recombinant_create(dataset_type='grantsmonthly', owner_org=org['name'])
+        rval = self.lc.action.recombinant_show(dataset_type='grantsmonthly', owner_org=org['name'])
+
+        self.resource_id = rval['resources'][0]['id']
+
+    def test_example(self):
+        record = get_chromo('grantsmonthly')['examples']['record']
+        self.lc.action.datastore_upsert(
+            resource_id=self.resource_id,
+            records=[record])
+
+    def test_blank(self):
+        with pytest.raises(ValidationError) as ve:
+            self.lc.action.datastore_upsert(
+                resource_id=self.resource_id,
+                records=[{}])
+        err = ve.value.error_dict
+        assert 'key' in err
+        assert 'ref_number, amendment_number' in err['key'][0]
