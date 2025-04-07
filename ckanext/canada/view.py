3c27547a9de0c5b1d7241bb27d17d90ccc32a1fa
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-04-07 19:27:36 +0000

    feat(templates): pd datatables cont;
    - Started adding different HTML input types for the datastore_types.

--- ckanext/canada/view.py
+++ ckanext/canada/view.py
@@ -94,22 +94,6 @@ class IntentionalServerError(Exception):
     pass
 
 
-def _url_part_escape(orig: str) -> str:
-    """
-    simple encoding for url-parts where all non-alphanumerics are
-    wrapped in e.g. _xxyyzz_ blocks w/hex UTF-8 xx, yy, zz values
-
-    used for safely including arbitrary unicode as part of a url path
-    all returned characters will be in [a-zA-Z0-9_-]
-    """
-    return '_'.join(
-        codecs.encode(s.encode('utf-8'), 'hex').decode('ascii') if i % 2 else s
-        for i, s in enumerate(
-            re.split(r'([^-a-zA-Z0-9]+)', orig)
-        )
-    )
-
-
 def _url_part_unescape(urlpart: str) -> str:
     """
     reverse url_part_escape
