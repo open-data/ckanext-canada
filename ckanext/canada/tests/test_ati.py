474c685f0ecbfa0e539feacf314117f0de6e1a24
gabmor38 <gabriela.morenor@gmail.com>
2025-06-25 09:20:04 -0400

    wip

--- ckanext/canada/tests/test_ati.py
+++ ckanext/canada/tests/test_ati.py
@@ -39,6 +39,111 @@ class TestAti(CanadaTestBase):
         assert 'key' in err
         assert 'request_number' in err['key'][0]
 
+    def test_request_number_comma(self):
+        with pytest.raises(ValidationError) as ve:
+            self.lc.action.datastore_upsert(
+                resource_id=self.resource_id,
+                records=[{
+                    'request_number': 'AA-2020-00,516',
+                    'disposition': 'DP',
+                    'month':'01',
+                    'pages':'1',
+                    'summary_en': 'summary',
+                    'summary_fr': 'summary french',
+                    'year': '2020'
+                          
+                }]
+            )
+        err = ve.value.error_dict['records'][0]
+        print('error', err)
+        assert 'request_number' in err
+
+    def test_year_before_2011(self):
+        with pytest.raises(ValidationError) as ve:
+            self.lc.action.datastore_upsert(
+                resource_id=self.resource_id,
+                records=[{
+                    'year': '2010',
+                    'request_number': 'AA-2020-00516',
+                    'disposition': 'DP',
+                    'month':'01',
+                    'pages':'1',
+                    'summary_en': 'summary',
+                    'summary_fr': 'summary french',
+                }]
+            )
+        err = ve.value.error_dict['records'][0]
+        assert 'year' in err
+
+    def test_year_future_date(self):
+        with pytest.raises(ValidationError) as ve:
+            self.lc.action.datastore_upsert(
+                resource_id=self.resource_id,
+                records=[{
+                    'year': '2029',
+                    'request_number': 'AA-2020-00516',
+                    'disposition': 'DP',
+                    'month':'01',
+                    'pages':'1',
+                    'summary_en': 'summary',
+                    'summary_fr': 'summary french',
+                }]
+            )
+        err = ve.value.error_dict['records'][0]
+        assert 'year' in err
+
+    def test_month_negative(self):
+        with pytest.raises(ValidationError) as ve:
+            self.lc.action.datastore_upsert(
+                resource_id=self.resource_id,
+                records=[{
+                    'month':'0',
+                    'year': '2029',
+                    'request_number': 'AA-2020-00516',
+                    'disposition': 'DP',
+                    'pages':'1',
+                    'summary_en': 'summary',
+                    'summary_fr': 'summary french',
+                }]
+            )
+        err = ve.value.error_dict['records'][0]
+        assert 'month' in err
+
+    def test_invalid_month(self):
+        with pytest.raises(ValidationError) as ve:
+            self.lc.action.datastore_upsert(
+                resource_id=self.resource_id,
+                records=[{
+                    'month':'13',
+                    'year': '2029',
+                    'request_number': 'AA-2020-00516',
+                    'disposition': 'DP',
+                    'pages':'1',
+                    'summary_en': 'summary',
+                    'summary_fr': 'summary french',
+                }]
+            )
+        err = ve.value.error_dict['records'][0]
+        assert 'month' in err
+
+    def test_invalid_month(self):
+        with pytest.raises(ValidationError) as ve:
+            self.lc.action.datastore_upsert(
+                resource_id=self.resource_id,
+                records=[{
+                    'month':'13',
+                    'year': '2029',
+                    'request_number': 'AA-2020-00516',
+                    'disposition': 'DP',
+                    'pages':'1',
+                    'summary_en': 'summary',
+                    'summary_fr': 'summary french',
+                }]
+            )
+        err = ve.value.error_dict['records'][0]
+        assert 'month' in err
+
+
 
 class TestAtiNil(CanadaTestBase):
     @classmethod
