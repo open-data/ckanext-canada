cbba5d6f80c8ac526a087cf3a613fe39630c5eac
RabiaSajjad <rabiasajjad.dev@gmail.com>
2025-12-12 16:39:17 -0500

    remove canada_maintainer_email_default output validator

--- ckanext/canada/plugin/theme_plugin.py
+++ ckanext/canada/plugin/theme_plugin.py
@@ -86,7 +86,6 @@ class CanadaThemePlugin(p.SingletonPlugin):
             'operations_guide_link',
             'max_resources_per_dataset',
             'support_email_address',
-            'default_open_email_address',
             'get_inline_script_nonce',
             'obfuscate_to_code_points',
             'mail_to'
