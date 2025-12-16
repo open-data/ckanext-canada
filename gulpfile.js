7a40d843c577647945f75b9e3e18bf6d1365d522
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2025-12-16 21:29:43 +0000

    refactor(css): sassify;
    - Moved canada custom css to scss;
    - Started working on custom datatables css to scss.
    - Removed source mapping from gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ const terser = require('gulp-terser');
 
 function compileCustomSass(){
   return src('ckanext/canada/assets/custom/source/*.scss')
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sass({style: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write())
     .pipe(rename(function(_fileObj){
       _fileObj.extname = '.min.css';
     }))
@@ -17,9 +17,9 @@ function compileCustomSass(){
 
 function compileDatatablesSass(){
   return src('ckanext/canada/assets/datatables/source/*.scss')
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sass({style: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write())
     .pipe(rename(function(_fileObj){
       _fileObj.extname = '.min.css';
     }))
@@ -28,9 +28,9 @@ function compileDatatablesSass(){
 
 function compileIMSass(){
   return src('ckanext/canada/assets/invitation-manager/vendor/*.css')
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sass({style: 'compressed'}).on('error', sass.logError))
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write())
     .pipe(rename(function(_fileObj){
       _fileObj.extname = '.min.css';
     }))
@@ -44,33 +44,33 @@ function watchSass(){
 }
 
 function minifyCustomJS(){
-  return src('ckanext/canada/assets/custom/source/*.js', {sourcemaps: true})
+  return src('ckanext/canada/assets/custom/source/*.js', {sourcemaps: false})
     .pipe(terser({'compress': true,
                   'mangle': true}))
     .pipe(rename(function(_fileObj){
       _fileObj.extname = '.min.js';
     }))
-    .pipe(dest('ckanext/canada/assets/custom/build', {sourcemaps: true}));
+    .pipe(dest('ckanext/canada/assets/custom/build', {sourcemaps: false}));
 }
 
 function minifyDatatablesJS(){
-  return src('ckanext/canada/assets/datatables/source/*.js', {sourcemaps: true})
+  return src('ckanext/canada/assets/datatables/source/*.js', {sourcemaps: false})
     .pipe(terser({'compress': true,
                   'mangle': true}))
     .pipe(rename(function(_fileObj){
       _fileObj.extname = '.min.js';
     }))
-    .pipe(dest('ckanext/canada/assets/datatables/build', {sourcemaps: true}));
+    .pipe(dest('ckanext/canada/assets/datatables/build', {sourcemaps: false}));
 }
 
 function minifyIMJS(){
-  return src('ckanext/canada/assets/invitation-manager/vendor/*.js', {sourcemaps: true})
+  return src('ckanext/canada/assets/invitation-manager/vendor/*.js', {sourcemaps: false})
     .pipe(terser({'compress': true,
                   'mangle': false}))
     .pipe(rename(function(_fileObj){
       _fileObj.extname = '.min.js';
     }))
-    .pipe(dest('ckanext/canada/assets/invitation-manager/build', {sourcemaps: true}));
+    .pipe(dest('ckanext/canada/assets/invitation-manager/build', {sourcemaps: false}));
 }
 
 function watchJS(){
