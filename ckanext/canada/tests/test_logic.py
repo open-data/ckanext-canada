b5f4f571b62e0d20b74762fbc89b5bea6b401ba4
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2026-02-03 19:30:33 +0000

    feat(test): coverage;
    - Test coverage for Resource positions.

--- ckanext/canada/tests/test_logic.py
+++ ckanext/canada/tests/test_logic.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
 from ckanext.canada.tests import CanadaTestBase
 from ckanapi import LocalCKAN
+from ckan import model
 
 from ckanext.canada.tests.factories import (
     CanadaResource as Resource,
@@ -246,3 +247,273 @@ class TestMetadataDatesLogic(CanadaTestBase):
         assert res['created'] == self.update_date
         assert res['last_modified'] == self.update_date
         assert res['metadata_modified'] == self.update_date
+
+
+class TestResourcePositionLogic(CanadaTestBase):
+    """
+    Tests that adding, updating, and patching Resources respect their
+    original position and do not magically get deleted.
+    """
+
+    @classmethod
+    def setup_class(self):
+        """Method is called at class level once the class is instatiated.
+        Setup any state specific to the execution of the given class.
+        """
+        super(TestResourcePositionLogic, self).setup_class()
+
+        sysadmin = Sysadmin(name='test_metadata_dates_sysadmin')
+        org = Organization(users=[{
+            'name': sysadmin['name'],
+            'capacity': 'admin'},])
+        self.sysadmin_action = LocalCKAN(username=sysadmin['name']).action
+
+        self.res_dict = {
+            'name_translated': {'en': 'Full text.', 'fr': 'Full text.'},
+            'format': 'TXT',
+            'url': 'http://www.annakarenina.com/download/',
+            'size': 42,
+            'resource_type': 'dataset',
+            'language': ['zxx'],
+            'created': '1994-01-01T00:00:01',
+            'last_modified': '1994-01-01T00:00:01',
+            'metadata_modified': '1994-01-01T00:00:01',
+        }
+
+        self.pkg_dict = {
+            'type': 'dataset',
+            'owner_org': org['name'],
+            'collection': 'primary',
+            'title_translated': {'en': 'A Novel By Tolstoy',
+                                 'fr': 'Un novel par Tolstoy'},
+            'frequency': 'as_needed',
+            'notes_translated': {'en': '...', 'fr': '...'},
+            'subject': ['persons'],
+            'date_published': '2013-01-01',
+            'keywords': {'en': ['book'], 'fr': ['livre']},
+            'license_id': 'ca-ogl-lgo',
+            'ready_to_publish': 'true',
+            'imso_approval': 'true',
+            'jurisdiction': 'federal',
+            'maintainer_email': 'not@all.example.com',
+            'restrictions': 'unrestricted',
+            'metadata_created': '1994-01-01T00:00:01',
+            'metadata_modified': '1994-01-01T00:00:01',
+        }
+
+    def test_auto_resource_positions(self):
+        """
+        Creating new Resources should just add them to the list of resources.
+        """
+        pkg = self.sysadmin_action.package_create(
+            name='76545678-9abc-def0-1234-56789abcd34c',
+            resources=[self.res_dict],
+            **self.pkg_dict)
+
+        assert len(pkg['resources']) == 1
+        assert pkg['resources'][0]['position'] == 0
+
+        self.sysadmin_action.resource_create(
+            package_id=pkg['id'],
+            **self.res_dict)
+
+        pkg = self.sysadmin_action.package_show(id=pkg['id'])
+
+        assert len(pkg['resources']) == 2
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+
+    def test_updating_resource_same_position(self):
+        """
+        Updating a resource should keep its previous position
+        """
+        pkg = self.sysadmin_action.package_create(
+            name='76545678-7ade-def0-1234-56789abcd34c',
+            resources=[self.res_dict, self.res_dict, self.res_dict,
+                       self.res_dict, self.res_dict, self.res_dict],
+            **self.pkg_dict)
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+        res_dict = pkg['resources'][3].copy()
+        res_dict['name_translated'] = {'en': 'UPDATED', 'fr': 'UPDATED'}
+
+        self.sysadmin_action.resource_update(**res_dict)
+
+        pkg = self.sysadmin_action.package_show(id=pkg['id'])
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][3]['name_translated'] == {'en': 'UPDATED', 'fr': 'UPDATED'}
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+        # you cannot update the position via update actions
+        res_dict = pkg['resources'][1].copy()
+        res_dict['name_translated'] = {'en': 'UPDATED AGAIN', 'fr': 'UPDATED AGAIN'}
+        res_dict['position'] = 99
+
+        self.sysadmin_action.resource_update(**res_dict)
+
+        pkg = self.sysadmin_action.package_show(id=pkg['id'])
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][1]['position'] != 99
+        assert pkg['resources'][1]['name_translated'] == {'en': 'UPDATED AGAIN', 'fr': 'UPDATED AGAIN'}
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][3]['name_translated'] == {'en': 'UPDATED', 'fr': 'UPDATED'}
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+    def test_patching_resource_same_position(self):
+        """
+        Patching a resource should keep its previous position
+        """
+        pkg = self.sysadmin_action.package_create(
+            name='391112e8-7ade-def0-1234-56789abcd34c',
+            resources=[self.res_dict, self.res_dict, self.res_dict,
+                       self.res_dict, self.res_dict, self.res_dict],
+            **self.pkg_dict)
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+        res_dict = pkg['resources'][3].copy()
+        res_dict['name_translated'] = {'en': 'UPDATED', 'fr': 'UPDATED'}
+
+        self.sysadmin_action.resource_patch(**res_dict)
+
+        pkg = self.sysadmin_action.package_show(id=pkg['id'])
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][3]['name_translated'] == {'en': 'UPDATED', 'fr': 'UPDATED'}
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+        # you cannot patch the position via patch actions
+        res_dict = pkg['resources'][1].copy()
+        res_dict['name_translated'] = {'en': 'UPDATED AGAIN', 'fr': 'UPDATED AGAIN'}
+        res_dict['position'] = 99
+
+        self.sysadmin_action.resource_patch(**res_dict)
+
+        pkg = self.sysadmin_action.package_show(id=pkg['id'])
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][1]['position'] != 99
+        assert pkg['resources'][1]['name_translated'] == {'en': 'UPDATED AGAIN', 'fr': 'UPDATED AGAIN'}
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][3]['name_translated'] == {'en': 'UPDATED', 'fr': 'UPDATED'}
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+    def test_delete_resource_assigns_positions(self):
+        """
+        Deleting a Resource from a package should reorder the other
+        Resources properly, and set the deleted position to 0.
+        """
+        pkg = self.sysadmin_action.package_create(
+            name='44cefde8-7cce-defc-1234-56789abcd34c',
+            resources=[self.res_dict, self.res_dict, self.res_dict,
+                       self.res_dict, self.res_dict, self.res_dict],
+            **self.pkg_dict)
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+        deleted_res_id = pkg['resources'][2]['id']
+
+        self.sysadmin_action.resource_delete(
+            id=deleted_res_id)
+
+        pkg = self.sysadmin_action.package_show(id=pkg['id'])
+
+        assert len(pkg['resources']) == 5
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][4]['position'] == 4
+        for r in pkg['resources']:
+            assert r['id'] != deleted_res_id
+
+        res = model.Resource.get(deleted_res_id)
+
+        assert res.id == deleted_res_id
+        assert res.position == 0
+
+    def test_reorder_resource_positions(self):
+        """
+        You can reorder Resources via package_resource_reorder action.
+        """
+        pkg = self.sysadmin_action.package_create(
+            name='391112e8-7cce-defc-1234-56789abcd34c',
+            resources=[self.res_dict, self.res_dict, self.res_dict,
+                       self.res_dict, self.res_dict, self.res_dict],
+            **self.pkg_dict)
+
+        assert len(pkg['resources']) == 6
+        assert pkg['resources'][0]['position'] == 0
+        assert pkg['resources'][1]['position'] == 1
+        assert pkg['resources'][2]['position'] == 2
+        assert pkg['resources'][3]['position'] == 3
+        assert pkg['resources'][4]['position'] == 4
+        assert pkg['resources'][5]['position'] == 5
+
+        original_order = [
+            pkg['resources'][0]['id'],
+            pkg['resources'][1]['id'],
+            pkg['resources'][2]['id'],
+            pkg['resources'][3]['id'],
+            pkg['resources'][4]['id'],
+            pkg['resources'][5]['id'],
+        ]
+
+        new_order = [
+            pkg['resources'][4]['id'],
+            pkg['resources'][5]['id'],
+            pkg['resources'][0]['id'],
+            pkg['resources'][2]['id'],
+            pkg['resources'][3]['id'],
+            pkg['resources'][1]['id'],
+        ]
+
+        self.sysadmin_action.package_resource_reorder(
+            id=pkg['id'],
+            order=new_order)
+
+        pkg = self.sysadmin_action.package_show(id=pkg['id'])
+
+        assert original_order != new_order
+        for i, r in enumerate(pkg['resources']):
+            assert r['position'] >= 0
+            assert r['id'] != original_order[i]
