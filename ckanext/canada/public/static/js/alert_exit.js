cec1dfc85c8ccd7a369d83ae05e7f7aa0892de34
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2026-08-20 13:38:05 -0400

    feat(misc): changelog;
    - Added change log file.

--- ckanext/canada/public/static/js/alert_exit.js
+++ ckanext/canada/public/static/js/alert_exit.js
@@ -1,9 +1,4 @@
 document.addEventListener('click', function(_event){
-    const language = document.documentElement.lang || 'en';
-    const trustedDomains = [
-      'canada.ca',
-      'gc.ca',
-    ];
     const link = _event.target.closest('a');
     if( ! link || ! link.href ){
       return;
@@ -14,6 +9,10 @@ document.addEventListener('click', function(_event){
     if( url.origin === window.location.origin ){
       return;
     }
+    const trustedDomains = [
+      'canada.ca',
+      'gc.ca',
+    ];
     const isTrusted = trustedDomains.some(function(_domain){
       return (url.hostname === _domain || url.hostname.endsWith('.' + _domain));
     });
@@ -22,6 +21,7 @@ document.addEventListener('click', function(_event){
     }
     _event.preventDefault();
     _event.stopImmediatePropagation();
+    const language = document.documentElement.lang || 'en';
     const modal = document.createElement('div');
     modal.className = 'modal show';
     modal.id = 'canada-external-site-warning';
@@ -35,24 +35,7 @@ document.addEventListener('click', function(_event){
     const modelContent = language === 'fr' ? "Le gouvernement du Canada n'est pas responsable de l'exactitude, de l'actualité, ni de la fiabilité du contenu de ce site externe. Il se peut également que ce dernier ne soit pas assujetti à la <em>Loi sur les langues officielles,</em> à la <em>Loi sur la protection des renseignements personnels,</em> ni à la <em>Loi canadienne sur l'accessibilité.</em>" : 'The Government of Canada is not responsible for the accuracy, timeliness or reliability of the content of this external site. This site may not be subject to the <em>Official Languages Act,</em> the <em>Privacy Act,</em> or the <em>Accessible Canada Act.</em>';
     const cancelButtonLabel = language === 'fr' ? 'Annuler' : 'Cancel';
     const confirmButtonLabel = language === 'fr' ? 'Quitter le site' : 'Leave Site';
-    modal.innerHTML = '\
-      <div class="modal-dialog"> \
-        <div class="modal-content"> \
-          <div class="modal-header"> \
-            <h3 class="modal-title" id="canada-external-site-warning--label"><span class="fa fa-warning" aria-hidden="true"></span>&nbsp;&nbsp;' + modalTitle + '</h3> \
-            <button type="button" class="btn-close" id="canada-external-site-warning--dismiss" aria-label="Close"></button> \
-          </div> \
-          <div class="modal-body"> \
-            <p><strong>' + modalLeadingContent + '</strong></p> \
-            <p>' + modelContent + '</p> \
-          </div> \
-          <div class="modal-footer"> \
-            <button class="btn btn-sm btn-secondary" id="canada-external-site-warning--cancel">' + cancelButtonLabel + '</button> \
-            <button class="btn btn-sm btn-warning" id="canada-external-site-warning--continue">' + confirmButtonLabel + '</button> \
-          </div> \
-        </div> \
-      </div> \
-    ';
+    modal.innerHTML = '<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h3 class="modal-title" id="canada-external-site-warning--label"><span class="fa fa-warning" aria-hidden="true"></span>&nbsp;&nbsp;' + modalTitle + '</h3><button type="button" class="btn-close" id="canada-external-site-warning--dismiss" aria-label="Close"></button></div><div class="modal-body"><p><strong>' + modalLeadingContent + '</strong></p><p>' + modelContent + '</p></div><div class="modal-footer"><button class="btn btn-sm btn-secondary" id="canada-external-site-warning--cancel">' + cancelButtonLabel + '</button><button class="btn btn-sm btn-warning" id="canada-external-site-warning--continue">' + confirmButtonLabel + '</button></div></div></div>';
     document.body.append(modal);
     modal.querySelector('#canada-external-site-warning--cancel').focus();
     modal.querySelector('#canada-external-site-warning--continue').addEventListener('click', function(){
